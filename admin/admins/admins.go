9e8c6c7c03fa6c747012e24d46c77cb4c546d7a8
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-06-14 20:07:33 +0300

    changed comments, deleted constructor

--- admin/admins/admins.go
+++ admin/admins/admins.go
@@ -9,20 +9,10 @@ import (
 	"github.com/google/uuid"
 )
 
-// Admin describes user entity.
+// Admin describes admin entity.
 type Admin struct {
 	ID           uuid.UUID
 	Email        string
 	PasswordHash []byte
 	CreatedAt    time.Time
-}
-
-// NewAdmin is constructor for Admin.
-func NewAdmin(email string,  password []byte) *Admin {
-	return &Admin{
-		ID:           uuid.New(),
-		Email:        email,
-		PasswordHash: password,
-		CreatedAt:    time.Now(),
-	}
 }
