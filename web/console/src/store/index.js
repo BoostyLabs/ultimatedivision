9279ccf82d24174fd260fd424b54f114c96a17f9
Oleh Perehuda <peregudson97@gmail.com>
2021-06-29 21:43:05 +0300

    changed state management for FilterField component

--- web/console/src/store/index.js
+++ web/console/src/store/index.js
@@ -6,11 +6,9 @@ See LICENSE for copying information.
 import { createStore, combineReducers } from 'redux';
 
 import { cardReducer } from './reducers/footballerCard';
-import { filterFieldTitlesReducer } from './reducers/filterFieldTitles';
 
 const reducer = combineReducers({
     footballerCard: cardReducer,
-    filterFieldTitles: filterFieldTitlesReducer
 });
 
 export const store = createStore(reducer);
