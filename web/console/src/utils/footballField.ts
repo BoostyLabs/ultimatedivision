2c488168f4338de11af88834766c13448538a25f
Vladyslav <vladdezhniuk@gmail.com>
2021-07-05 23:50:19 +0300

    footballField formation selection

--- web/console/src/utils/footballField.ts
+++ web/console/src/utils/footballField.ts
@@ -1,29 +1,20 @@
-export class DropDownStyles {
-    constructor(
-        public state: boolean = false,
-    ) { }
-    get style(): string {
-        let style: string ='';
-        return style;
-    }
+export interface DropDownStyles {
+    state: boolean;
+    get style(): string;
 }
-export class TriangleStyle extends DropDownStyles {
+export class TriangleStyle implements DropDownStyles {
     constructor(
         public state: boolean = false
-    ) {
-        super(state)
-    }
+    ) { }
     get style() {
         let style = this.state ? 'rotate(-90deg)' : 'rotate(0deg)';
         return style;
     }
 }
-export class ListStyle extends DropDownStyles {
+export class ListStyle implements DropDownStyles {
     constructor(
         public state: boolean = false
-    ) {
-        super(state)
-    }
+    ) { }
     get style() {
         let style = this.state ? '0' : '90px';
         return style;
