98b908a844d261e6202a9578e0eeee7dde614132
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-07-06 20:13:11 +0300

    database/cards: add check error in transaction rollback

--- database/cards.go
+++ database/cards.go
@@ -45,7 +45,6 @@ const (
 
 // Create add card in the data base.
 func (cardsDB *cardsDB) Create(ctx context.Context, card cards.Card) error {
-
 	tx, err := cardsDB.conn.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -59,6 +58,7 @@ func (cardsDB *cardsDB) Create(ctx context.Context, card cards.Card) error {
 			$26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49,
 			$50, $51, $52, $53, $54, $55, $56, $57, $58, $59, $60)
 		`
+
 	_, err = cardsDB.conn.ExecContext(ctx, query,
 		card.DominantFoot, card.UserID, card.Tactics, card.Positioning, card.Composure, card.Aggression, card.Vision, card.Awareness,
 		card.Crosses, card.Physique, card.Acceleration, card.RunningSpeed, card.ReactionSpeed, card.Agility, card.Stamina, card.Strength,
@@ -68,15 +68,20 @@ func (cardsDB *cardsDB) Create(ctx context.Context, card cards.Card) error {
 		card.Sliding, card.Tackles, card.BallFocus, card.Interceptions, card.Vigilance, card.Goalkeeping, card.Reflexes, card.Diving,
 		card.Handling, card.Sweeping, card.Throwing,
 	)
-
 	if err != nil {
-		tx.Rollback()
+		err = tx.Rollback()
+		if err != nil {
+			return err
+		}
 		return ErrCard.Wrap(err)
 	}
 
 	err = createCardsAccessories(ctx, cardsDB, card)
 	if err != nil {
-		tx.Rollback()
+		err = tx.Rollback()
+		if err != nil {
+			return err
+		}
 		return ErrAccessory.Wrap(err)
 	}
 
