7be9de3f8663f4f7b4db748d13d8cae9e53d5169
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-07-19 18:27:54 +0300

    cards/common: add Action entity

--- database/common.go
+++ database/common.go
@@ -7,6 +7,20 @@ import (
 	"strings"
 )
 
+// Action defines the list of possible filter actions.
+type Action string
+
+const (
+	// EQ - equal to value.
+	EQ Action = "="
+	// GTE - greater than or equal to value.
+	GTE Action = ">="
+	// LTE - less than or equal to value.
+	LTE Action = "<="
+	// LIKE - like to value.
+	LIKE Action = "LIKE"
+)
+
 // normalizeEmail brings the email to UpperCase.
 func normalizeEmail(email string) string {
 	return strings.ToUpper(email)
