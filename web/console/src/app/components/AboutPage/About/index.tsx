7263ed17d0237f1c89f9ab3d9007c4069148be78
vladdezhniuk <vladdezhniuk@gmail.com>
2021-07-30 14:05:38 +0300

    route changes

--- web/console/src/app/components/AboutPage/About/index.tsx
+++ web/console/src/app/components/AboutPage/About/index.tsx
@@ -1,29 +1,28 @@
-//Copyright (C) 2021 Creditor Corp. Group.
-//See LICENSE for copying information.
-
+// Copyright (C) 2021 Creditor Corp. Group.
+// See LICENSE for copying information.
 import { Switch } from 'react-router-dom';
-
-import { ComponentRoutes, Route } from '@/app/routes';
+import { AboutPageSubroutes, ComponentRoutes, Route } from '@/app/routes';
 import { AboutMenu } from '../AboutMenu';
-
 import './index.scss';
-
-const About: React.FC<{ children: ComponentRoutes[] }> = ({ children }) =>
-    <div className="about">
-        <AboutMenu />
-        <div className="about__wrapper">
-            <Switch>
-                {children.map((item: ComponentRoutes, index: number) =>
-                    <Route
-                        key={index}
-                        path={item.path}
-                        component={item.component}
-                        exact={item.exact}
-                    />
-                )}
-            </Switch>
+const About: React.FC<{ children: AboutPageSubroutes }> = ({ children }) => {
+    const { whitePaper, tokenomics } = children;
+    const routes = whitePaper.concat(tokenomics);
+    return (
+        <div className="about">
+            <AboutMenu />
+            <div className="about__wrapper">
+                <Switch>
+                    {routes.map((item: ComponentRoutes, index: number) =>
+                        <Route
+                            key={index}
+                            path={item.path}
+                            component={item.component}
+                            exact={item.exact}
+                        />
+                    )}
+                </Switch>
+            </div>
         </div>
-    </div>;
-
-
+    );
+};
 export default About;
