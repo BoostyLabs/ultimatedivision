37a406da4865fb2003d15ed33b71c887a43eb6c4
vladdezhniuk <vladdezhniuk@gmail.com>
2021-08-31 17:06:05 +0300

    Lootbox page implementation

--- web/console/src/app/components/Store/LootboxContent/LootboxOpening/index.tsx
+++ web/console/src/app/components/Store/LootboxContent/LootboxOpening/index.tsx
@@ -5,16 +5,14 @@ import { MyCard } from '@/app/components/Club/ClubCardsArea/MyCard';
 import { RootState } from '@/app/store';
 import boxBody from '@static/img/StorePage/BoxContent/boxBody.svg';
 import boxCover from '@static/img/StorePage/BoxContent/boxCover.svg';
-import leftLight from '@static/img/StorePage/BoxContent/leftLight.svg';
-import rightLight from '@static/img/StorePage/BoxContent/rightLight.svg';
-import centerLight from '@static/img/StorePage/BoxContent/centerLight.svg';
+import boxLight from '@static/img/StorePage/BoxContent/boxLight.svg';
 import ribbons from '@static/img/StorePage/BoxContent/ribbons.svg';
 import { useSelector } from 'react-redux';
 
 import './index.scss';
 
 
-export const BoxAnimation = () => {
+export const LootboxOpening = () => {
     // TODO: will be replaced by backend data
     // eslint-disable-next-line
     const card = useSelector((state: RootState) => state.cardsReducer.cards[0]);
@@ -32,23 +30,13 @@ export const BoxAnimation = () => {
                     alt="box cover"
                     className="box-animation__box-cover"
                 />
-                <div className="box-animation__light">
-                    <img
-                        src={leftLight}
-                        alt="shadow image"
-                        className="box-animation__light-image"
-                    />
-                    <img
-                        src={centerLight}
-                        alt="shadow image"
-                        className="box-animation__light-image"
-                    />
-                    <img
-                        src={rightLight}
-                        alt="shadow image"
-                        className="box-animation__light-image"
-                    />
-                </div>
+
+                <img
+                    src={boxLight}
+                    alt="shadow image"
+                    className="box-animation__light"
+                />
+
                 <img
                     src={ribbons}
                     alt="ribbons"
@@ -57,7 +45,9 @@ export const BoxAnimation = () => {
             </div>
             <div className="box-animation__card-wrapper">
                 <div className="box-animation__card-wrapper-backlight">
-                    <MyCard card={card} />
+                    {card &&
+                        <MyCard card={card} />
+                    }
                 </div>
             </div>
         </div>
--- web/console/src/app/router/index.tsx
+++ web/console/src/app/router/index.tsx
@@ -11,7 +11,6 @@ const FootballField = lazy(() => import('@/app/views/FootballFieldPage'));
 const WhitePaper = lazy(() => import('@/app/views/WhitePaperPage'));
 const Tokenomics = lazy(() => import('@/app/views/TokenomicsPage'));
 const Store = lazy(() => import('@/app/views/StorePage'));
-const BoxContent = lazy(() => import('@components/Store/BoxContent'));
 
 import Summary from '@components/WhitePaper/Summary';
 import GameMechanics from '@components/WhitePaper/GameMechanics';
@@ -75,11 +74,6 @@ export class RouteConfig {
     public static Store: ComponentRoutes = new ComponentRoutes(
         '/store',
         Store,
-        true,
-    );
-    public static BoxContent: ComponentRoutes = new ComponentRoutes(
-        '/content',
-        BoxContent,
         true,
     );
     public static Club: ComponentRoutes = new ComponentRoutes(
@@ -149,7 +143,6 @@ export class RouteConfig {
         RouteConfig.Club,
         RouteConfig.FootballerCard,
         RouteConfig.Store,
-        RouteConfig.BoxContent,
         RouteConfig.Whitepaper.addChildren([
             RouteConfig.Summary,
             RouteConfig.GameMechanick,
