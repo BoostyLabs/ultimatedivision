90e5d7dc9100699fd40cd982cd3a05e85114b05a
vladdezhniuk <vladdezhniuk@gmail.com>
2021-09-02 12:56:50 +0300

    hook fix

--- web/console/src/app/hooks/cards.tsx
+++ web/console/src/app/hooks/cards.tsx
@@ -1,34 +1,20 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { Card } from '@/card';
-import { SetStateAction, useEffect, useState } from 'react';
+import { useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 
 export const useCards = (thunk: any) => {
-    type Data = {
-        data: null | Card[];
-        isLoading: boolean;
-    };
 
     const dispatch = useDispatch();
-
-    const [data, handleData] = useState<SetStateAction<Data>>({ data: null, isLoading: true });
 
     /** Calls method get from  ClubClient */
     async function getDataFromApi() {
         await dispatch(thunk());
-
-        handleData({
-            // @ts-ignore
-            data: cards,
-            isLoading: false,
-        });
     };
 
     useEffect(() => {
         getDataFromApi();
     }, []);
 
-    return data;
 };
