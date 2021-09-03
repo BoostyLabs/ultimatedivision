30e7d5477a26a6523fd65cdbc4a2d70afd56f861
vladdezhniuk <vladdezhniuk@gmail.com>
2021-09-03 14:22:12 +0300

    redux changes according to goServer

--- web/console/src/app/store/actions/cards.ts
+++ web/console/src/app/store/actions/cards.ts
@@ -4,10 +4,15 @@
 export const GET_USER_CARDS = ' GET_CARDS';
 export const GET_SELLING_CARDS = ' GET_CARDS';
 import { CardClient } from '@/api/cards';
+import { Card } from '@/card';
 import { CardService } from '@/card/service';
 import { Dispatch } from 'redux';
 
-export const getCards = (cards: []) => ({
+export const getUserCards = (cards: []) => ({
+    type: GET_USER_CARDS,
+    action: cards,
+});
+export const getSellingCards = (cards: []) => ({
     type: GET_USER_CARDS,
     action: cards,
 });
@@ -19,13 +24,11 @@ const service = new CardService(client);
 export const userCards = () => async function(dispatch: Dispatch) {
     const response = await service.getUserCards();
     const cards = await response.json();
-
-    await dispatch(getCards(cards));
+    await dispatch(getUserCards(cards.map((card: Card) => new Card(card))));
 };
 /** thunk for creating user cards list */
 export const marketplaceCards = () => async function(dispatch: Dispatch) {
     const response = await service.getSellingCards();
     const cards = await response.json();
-
-    await dispatch(getCards(cards));
+    await dispatch(getSellingCards(cards.map((card: Card) => new Card(card))));
 };
--- web/console/src/card/index.ts
+++ web/console/src/card/index.ts
@@ -1,33 +1,33 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
-
+​
 import diamond from '@static/img/MarketPlacePage/marketPlaceCardsGroup/diamond2.svg';
 import gold from '@static/img/MarketPlacePage/marketPlaceCardsGroup/gold2.svg';
 import silver from '@static/img/MarketPlacePage/marketPlaceCardsGroup/silver2.svg';
 import wood from '@static/img/MarketPlacePage/marketPlaceCardsGroup/wood2.svg';
-
+​
 import diamondShadow from '@static/img/MarketPlacePage/marketPlaceCardsGroup/diamondShadow.svg';
 import goldShadow from '@static/img/MarketPlacePage/marketPlaceCardsGroup/goldShadow.svg';
 import silverShadow from '@static/img/MarketPlacePage/marketPlaceCardsGroup/silverShadow.svg';
 import woodShadow from '@static/img/MarketPlacePage/marketPlaceCardsGroup/woodShadow.svg';
-
+​
 import currentBid
     from '@static/img/MarketPlacePage/marketPlaceCardsGroup/marketPlaceFootballerCard/bid.svg';
 import minimumPrice
     from '@static/img/MarketPlacePage/marketPlaceCardsGroup/marketPlaceFootballerCard/minimum.svg';
 import purchased
     from '@static/img/MarketPlacePage/marketPlaceCardsGroup/marketPlaceFootballerCard/purchased.svg';
-
+​
 import confirmIcon from '@static/img/MarketPlacePage/MyCard/ok.svg';
 import priceGoldIcon from '@static/img/MarketPlacePage/MyCard/goldPrice.svg';
 import playerFace from '@static/img/MarketPlacePage/marketPlaceCardsGroup/player.svg';
 import priceIcon
     from '@static/img/MarketPlacePage/marketPlaceCardsGroup/marketPlaceFootballerCard/price.svg';
-
+​
 import checked from '@static/img/FootballerCardPage/checked.svg';
 import star from '@static/img/FootballerCardPage/star.svg';
-
-
+​
+​
 /** class for our getters to get label and value while mapping */
 export class CardField {
     /** label and value for mapping */
@@ -36,7 +36,7 @@ export class CardField {
         public value: string | number,
     ) { }
 }
-
+​
 /* eslint-disable */
 /** player stats implementation */
 export class CardStats {
@@ -46,9 +46,9 @@ export class CardStats {
         public abbreviated: string = '',
         public fields: CardField[] = []
     ) { }
-    public average: number = this.fields
+    public average: number = Math.round(this.fields
         .map(item => +item.value)
-        .reduce((prev, current) => prev + current) / this.fields.length;
+        .reduce((prev, current) => prev + current) / this.fields.length);
     /** abbreviated title of card stat name */
     get abbr(): string {
         return this.title.slice(0, 3);
@@ -57,11 +57,11 @@ export class CardStats {
     get color(): string {
         const STATISTIC_UPPER_BOUND = 90;
         const STATISTIC_LOWER_BOUND = 50;
-
+​
         const STATISTIC_UPPER_BOUND_COLOR = '#3CCF5D';
         const STATISTIC_MEDIUM_BOUND_COLOR = '#E8EC16';
         const STATISTIC_LOWER_BOUND_COLOR = '#FF4200';
-
+​
         switch (true) {
             case this.average >= STATISTIC_UPPER_BOUND:
                 return STATISTIC_UPPER_BOUND_COLOR;
@@ -72,79 +72,213 @@ export class CardStats {
         }
     }
 }
-
+​
+interface CardInterface {
+    id: string,
+    playerName: string,
+    quality: string,
+    pictureType: number,
+    height: number,
+    weight: number,
+    skinColor: number,
+    hairStyle: number,
+    hairColor: number,
+    accessories: number[],
+    dominantFoot: string,
+    isTatoos: boolean,
+    status: number,
+    type: string,
+    userId: string,
+    tactics: number,
+    positioning: number,
+    composure: number,
+    aggression: number,
+    vision: number,
+    awareness: number,
+    crosses: number,
+    physique: number,
+    acceleration: number,
+    runningSpeed: number,
+    reactionSpeed: number,
+    agility: number,
+    stamina: number,
+    strength: number,
+    jumping: number,
+    balance: number,
+    technique: number,
+    dribbling: number,
+    ballControl: number,
+    weakFoot: number,
+    skillMoves: number,
+    finesse: number,
+    curve: number,
+    volleys: number,
+    shortPassing: number,
+    longPassing: number,
+    forwardPass: number,
+    offense: number,
+    finishingAbility: number,
+    shotPower: number,
+    accuracy: number,
+    distance: number,
+    penalty: number,
+    freeKicks: number,
+    corners: number,
+    headingAccuracy: number,
+    defence: number,
+    offsideTrap: number,
+    sliding: number,
+    tackles: number,
+    ballFocus: number,
+    interceptions: number,
+    vigilance: number,
+    goalkeeping: number,
+    reflexes: number,
+    diving: number,
+    handling: number,
+    sweeping: number,
+    throwing: number
+}
+​
 /** Card base implementation */
 export class Card {
+    id: string;
+    playerName: string;
+    quality: string;
+    pictureType: number;
+    height: number;
+    weight: number;
+    skinColor: number;
+    hairStyle: number;
+    hairColor: number;
+    accessories: number[];
+    dominantFoot: string;
+    isTatoos: boolean;
+    status: number;
+    type: string;
+    userId: string;
+    tactics: number;
+    positioning: number;
+    composure: number;
+    aggression: number;
+    vision: number;
+    awareness: number;
+    crosses: number;
+    physique: number;
+    acceleration: number;
+    runningSpeed: number;
+    reactionSpeed: number;
+    agility: number;
+    stamina: number;
+    strength: number;
+    jumping: number;
+    balance: number;
+    technique: number;
+    dribbling: number;
+    ballControl: number;
+    weakFoot: number;
+    skillMoves: number;
+    finesse: number;
+    curve: number;
+    volleys: number;
+    shortPassing: number;
+    longPassing: number;
+    forwardPass: number;
+    offense: number;
+    finishingAbility: number;
+    shotPower: number;
+    accuracy: number;
+    distance: number;
+    penalty: number;
+    freeKicks: number;
+    corners: number;
+    headingAccuracy: number;
+    defence: number;
+    offsideTrap: number;
+    sliding: number;
+    tackles: number;
+    ballFocus: number;
+    interceptions: number;
+    vigilance: number;
+    goalkeeping: number;
+    reflexes: number;
+    diving: number;
+    handling: number;
+    sweeping: number;
+    throwing: number;
     /** Card fields */
     constructor(
-        public id: string,
-        public playerName: string,
-        public quality: string,
-        public pictureType: number,
-        public height: number,
-        public weight: number,
-        public skinColor: number,
-        public hairStyle: number,
-        public hairColor: number,
-        public accessories: number[],
-        public dominantFoot: string,
-        public isTatoos: boolean,
-        public status: number,
-        public type: string,
-        public userId: string,
-        public tactics: number,
-        public positioning: number,
-        public composure: number,
-        public aggression: number,
-        public vision: number,
-        public awareness: number,
-        public crosses: number,
-        public physique: number,
-        public acceleration: number,
-        public runningSpeed: number,
-        public reactionSpeed: number,
-        public agility: number,
-        public stamina: number,
-        public strength: number,
-        public jumping: number,
-        public balance: number,
-        public technique: number,
-        public dribbling: number,
-        public ballControl: number,
-        public weakFoot: number,
-        public skillMoves: number,
-        public finesse: number,
-        public curve: number,
-        public volleys: number,
-        public shortPassing: number,
-        public longPassing: number,
-        public forwardPass: number,
-        public offense: number,
-        public finishingAbility: number,
-        public shotPower: number,
-        public accuracy: number,
-        public distance: number,
-        public penalty: number,
-        public freeKicks: number,
-        public corners: number,
-        public headingAccuracy: number,
-        public defence: number,
-        public offsideTrap: number,
-        public sliding: number,
-        public tackles: number,
-        public ballFocus: number,
-        public interceptions: number,
-        public vigilance: number,
-        public goalkeeping: number,
-        public reflexes: number,
-        public diving: number,
-        public handling: number,
-        public sweeping: number,
-        public throwing: number,
-    ) { }
-
+        public card: CardInterface
+    ) {
+        this.id = card.id
+        this.playerName = card.playerName
+        this.quality = card.quality
+        this.pictureType = card.pictureType
+        this.height = card.height
+        this.weight = card.weight
+        this.skinColor = card.skinColor
+        this.hairStyle = card.hairStyle
+        this.hairColor = card.hairColor
+        this.accessories = card.accessories
+        this.dominantFoot = card.dominantFoot
+        this.isTatoos = card.isTatoos
+        this.status = card.status
+        this.type = card.type
+        this.userId = card.userId
+        this.tactics = card.tactics
+        this.positioning = card.positioning
+        this.composure = card.composure
+        this.aggression = card.aggression
+        this.vision = card.vision
+        this.awareness = card.awareness
+        this.crosses = card.crosses
+        this.physique = card.physique
+        this.acceleration = card.acceleration
+        this.runningSpeed = card.runningSpeed
+        this.reactionSpeed = card.reactionSpeed
+        this.agility = card.agility
+        this.stamina = card.stamina
+        this.strength = card.strength
+        this.jumping = card.jumping
+        this.balance = card.balance
+        this.technique = card.technique
+        this.dribbling = card.dribbling
+        this.ballControl = card.ballControl
+        this.weakFoot = card.weakFoot
+        this.skillMoves = card.skillMoves
+        this.finesse = card.finesse
+        this.curve = card.curve
+        this.volleys = card.volleys
+        this.shortPassing = card.shortPassing
+        this.longPassing = card.longPassing
+        this.forwardPass = card.forwardPass
+        this.offense = card.offense
+        this.finishingAbility = card.finishingAbility
+        this.shotPower = card.shotPower
+        this.accuracy = card.accuracy
+        this.distance = card.distance
+        this.penalty = card.penalty
+        this.freeKicks = card.freeKicks
+        this.corners = card.corners
+        this.headingAccuracy = card.headingAccuracy
+        this.defence = card.defence
+        this.offsideTrap = card.offsideTrap
+        this.sliding = card.sliding
+        this.tackles = card.tackles
+        this.ballFocus = card.ballFocus
+        this.interceptions = card.interceptions
+        this.vigilance = card.vigilance
+        this.goalkeeping = card.goalkeeping
+        this.reflexes = card.reflexes
+        this.diving = card.diving
+        this.handling = card.handling
+        this.sweeping = card.sweeping
+        this.throwing = card.throwing
+    }
+​
     /** returns background type and shadow type according to quality */
     get style() {
+​
         switch (this.quality) {
             case 'wood':
                 return {
@@ -166,13 +300,18 @@ export class Card {
                     background: diamond,
                     shadow: diamondShadow,
                 };
+            default:
+                return {
+                    background: diamond,
+                    shadow: diamondShadow,
+                };
         };
     }
     /** will be replaced by backend face implementation */
     get face() {
         return playerFace
     }
-
+​
     /**TODO: for testing, will be replaced */
     get cardPrice() {
         const prp = 75;
@@ -183,13 +322,13 @@ export class Card {
         const PRICE_UPPER_BOUND = 80;
         const PRICE_MEDIUM_BOUND = 70;
         const PRICE_LOWER_BOUND = 50;
-
+​
         const PRICE_UPPER_BOUND_COLOR = '#1898D7';
         const PRICE_MEDIUM_BOUND_COLOR = '#3CCF5D';
         const PRICE_LOWER_BOUND_COLOR = '#E86C27';
         const PRICE_DEFAULT_BOUND_COLOR = '#FF4200';
         let color: string;
-
+​
         switch (true) {
             case prp >= PRICE_UPPER_BOUND:
                 color = PRICE_UPPER_BOUND_COLOR;
@@ -203,7 +342,7 @@ export class Card {
             default:
                 color = PRICE_DEFAULT_BOUND_COLOR;
         }
-
+​
         return {
             prp,
             color,
@@ -212,7 +351,7 @@ export class Card {
             price
         }
     }
-
+​
     /** Using in footballerCard in info block */
     get infoBlock() {
         return [
@@ -238,7 +377,7 @@ export class Card {
             new CardField('age', '36 years old'),
         ];
     }
-
+​
     /** Using in diagramm area in footballerCard */
     get diagramArea() {
         // TODO: need to get real min and max values to convert into diagram value
@@ -252,7 +391,7 @@ export class Card {
             new CardField('in game stats', 688),
         ];
     }
-
+​
     /** returns fields for card stats area in footballerCard */
     get statsArea() {
         return [
