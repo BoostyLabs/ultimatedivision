6af17874b251f44ebf7703ed34b6283fa346c59f
PTaras <potsikaylot@gmail.com>
2021-09-07 17:32:47 +0300

    fixed buy Lootbox method

--- web/console/src/app/components/Store/LootboxSelection/LootboxCard/index.tsx
+++ web/console/src/app/components/Store/LootboxSelection/LootboxCard/index.tsx
@@ -41,7 +41,7 @@ export const LootboxCard: React.FC<{ data: LootboxStats; handleOpening: Dispatch
 
     const handleAnimation = () => {
         handleOpening(true);
-        dispatch(openLootbox({ uuid: data.id, name: data.title }));
+        dispatch(openLootbox({ uuid: data.id, type: data.type}));
     };
 
     return (
@@ -52,7 +52,7 @@ export const LootboxCard: React.FC<{ data: LootboxStats; handleOpening: Dispatch
                         className="box-card__icon"
                         style={{ backgroundImage: `url(${data.icon})` }}
                     />
-                    <h2 className="box-card__title">{data.title}</h2>
+                    <h2 className="box-card__title">{data.type}</h2>
                     <div className="box-card__quantity">
                         <span className="box-card__quantity-label">Cards</span>
                         <span className="box-card__quantity-value">{data.quantity}</span>
