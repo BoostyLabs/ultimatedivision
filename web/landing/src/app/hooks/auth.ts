76f182d79cbbca82f078d19db79a62c5f56ff155
Oleh Perehuda <peregudson97@gmail.com>
2021-09-14 16:46:50 +0300

    reworked useAuth hook

--- web/landing/src/app/hooks/auth.ts
+++ web/landing/src/app/hooks/auth.ts
@@ -1,25 +1,36 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
+import { useEffect } from "react";
+import { useHistory, useLocation } from "react-router"
+
 import { UserClient } from "@/api/user";
 import { UserService } from "@/user/service";
-import { useEffect } from "react";
-import { useLocation } from "react-router"
+import { RouteConfig } from "../router";
 
 export const useAuth = () => {
     useEffect(() => {
+        let token = query.get('token');
+        if (token) {
+            localStorage.setItem('recoverPassword', token);
+            history.replace({
+                pathname: RouteConfig.RecoverPassword.path,
+            });
+        };
+
         checkToken();
     }, []);
 
+    const history = useHistory();
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     };
-
     const query = useQuery();
+
     const userClient = new UserClient();
     const users = new UserService(userClient);
 
     async function checkToken() {
-        return await users.checkToken(query.get('token'));
+        return await users.checkToken(localStorage.getItem('recoverPassword'));
     };
 };
