a4526030a6e8e718e405661352abeef411336c00
Oleh Perehuda <peregudson97@gmail.com>
2021-09-15 17:11:23 +0300

    added variables

--- web/landing/src/app/components/WelcomePage/LaunchRoadmap/Card/index.tsx
+++ web/landing/src/app/components/WelcomePage/LaunchRoadmap/Card/index.tsx
@@ -15,56 +15,60 @@ export const Card: React.FC<{
         value: number,
     }
 }> = ({ card }) => {
+    const LOWER_BREAKPOINT = card.value;
+    const UPPER_BREAKPOINT = 100 - LOWER_BREAKPOINT;
+    const LOWER_BREAKPOINT_COLOR = '#37FB63';
+    const UPPER_BREAKPOINT_COLOR = '#323c92';
     return (
         <div className="card">
-                <h1 className="card__title">
-                    {card.title}
-                </h1>
-                <div className="card__diagram">
-                    <Doughnut data={{
-                        labels: [],
-                        datasets: [{
-                            data: [card.value, 100 - card.value],
-                            backgroundColor: [
-                                '#37FB63',
-                                '#323c92'
-                            ],
-                            borderColor: 'transparent',
-                        }],
-                        hoverOffset: 16,
-                    }}
-                        options={{
-                            plugins: {
-                                tooltip: {
-                                    backgroundColor: 'transparent',
-                                    displayColors: true,
-                                    padding: {
-                                        left: 135,
-                                        right: 355,
-                                        top: 270,
-                                        bottom: 280,
-                                    },
+            <h1 className="card__title">
+                {card.title}
+            </h1>
+            <div className="card__diagram">
+                <Doughnut data={{
+                    labels: [],
+                    datasets: [{
+                        data: [LOWER_BREAKPOINT, UPPER_BREAKPOINT],
+                        backgroundColor: [
+                            LOWER_BREAKPOINT_COLOR,
+                            UPPER_BREAKPOINT_COLOR,
+                        ],
+                        borderColor: 'transparent',
+                    }],
+                    hoverOffset: 16,
+                }}
+                    options={{
+                        plugins: {
+                            tooltip: {
+                                backgroundColor: 'transparent',
+                                displayColors: false,
+                                padding: {
+                                    left: 135,
+                                    right: 355,
+                                    top: 270,
+                                    bottom: 280,
                                 },
                             },
-                        }}
-                    />
-                    <p className="card__diagram__value">
-                        {card.value}%
-                    </p>
-                </div>
-                <p className="card__description">
-                    {card.description}
+                        },
+                    }}
+                />
+                <p className="card__diagram__value">
+                    {card.value}%
                 </p>
-                <div className="card__box">
-                    <img
-                        className="card__box__present"
-                        src={box}
-                        alt="utlimate box"
-                    />
-                    <p className="card__box__subtitle">
-                        {card.subTitle}
-                    </p>
-                </div>
             </div>
+            <p className="card__description">
+                {card.description}
+            </p>
+            <div className="card__box">
+                <img
+                    className="card__box__present"
+                    src={box}
+                    alt="utlimate box"
+                />
+                <p className="card__box__subtitle">
+                    {card.subTitle}
+                </p>
+            </div>
+        </div>
     )
 };
