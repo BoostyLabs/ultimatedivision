2024c9670f3eab324bad326f0e69caf90633a4ea
Oleh Perehuda <peregudson97@gmail.com>
2021-09-16 13:14:59 +0300

    useConfirmEmail hook reworked

--- web/landing/src/app/hooks/confirmEmail.ts
+++ web/landing/src/app/hooks/confirmEmail.ts
@@ -2,26 +2,16 @@
 // See LICENSE for copying information.
 
 import { useEffect } from "react";
-import { useHistory, useLocation } from "react-router";
+import { useLocation } from "react-router";
 
 import { UserService } from "@/user/service";
 import { UserClient } from "@/api/user";
-import { RouteConfig } from "../router";
 
 export const useConfirmEmail = () => {
     useEffect(() => {
-        let token = query.get("token");
-        if (token) {
-            localStorage.setItem('confirmEmail', token);
-            history.replace({
-                pathname: RouteConfig.ConfirmEmail.path,
-            });
-        };
-
         confirmEmail();
     }, []);
 
-    const history = useHistory();
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     };
@@ -31,6 +21,6 @@ export const useConfirmEmail = () => {
     const users = new UserService(userClient);
 
     async function confirmEmail() {
-        return await users.confirmEmail(localStorage.getItem('confirmEmail'));
+        return await users.confirmEmail(query.get('token'));
     };
 };
