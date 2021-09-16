bc8307eb881d2dd67551906d82a872bde3142a00
Oleh Perehuda <peregudson97@gmail.com>
2021-09-16 16:17:46 +0300

    reworked hooks

--- web/landing/src/app/views/ConfirmEmail/index.tsx
+++ web/landing/src/app/views/ConfirmEmail/index.tsx
@@ -1,11 +1,39 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { useConfirmEmail } from '@/app/hooks/confirmEmail';
+import { SetStateAction, useEffect, useState } from 'react';
+
+import { UserClient } from '@/api/user';
+import { UserService } from '@/user/service';
+
+import { useQueryToken } from '@/app/hooks/useQueryToken';
 
 /** TODO: Rework this view after design solution */
 const ConfirmEmail: React.FC = () => {
-    useConfirmEmail();
+    useEffect(() => {
+        checkEmailToken();
+    }, []);
+    const token = useQueryToken();
+
+    const [errorMessage, setErrorMessage] =
+        useState<SetStateAction<null | string>>(null);
+
+    const userClient = new UserClient();
+    const users = new UserService(userClient);
+    /** catches error if token is not valid */
+    async function checkEmailToken() {
+        try {
+            await users.checkEmailToken(token);
+        } catch (error: any) {
+            /** TODO: handles error */
+            setErrorMessage('PAGE 404 NOT FOUND');
+        };
+    };
+
+    if (errorMessage) {
+        return <h1>{errorMessage}</h1>
+    };
+
     return <div>
         <h1>Your email has been successfully verified</h1>
     </div>;
