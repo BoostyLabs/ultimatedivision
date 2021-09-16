cd4107a5079a15deb847888d4dc27ff609eb5183
Oleh Perehuda <peregudson97@gmail.com>
2021-09-16 13:06:14 +0300

    useAuth hook changed

--- web/landing/src/app/hooks/auth.ts
+++ web/landing/src/app/hooks/auth.ts
@@ -2,26 +2,16 @@
 // See LICENSE for copying information.
 
 import { useEffect } from "react";
-import { useHistory, useLocation } from "react-router"
+import { useLocation } from "react-router"
 
 import { UserClient } from "@/api/user";
 import { UserService } from "@/user/service";
-import { RouteConfig } from "../router";
 
 export const useAuth = () => {
     useEffect(() => {
-        let token = query.get('token');
-        if (token) {
-            localStorage.setItem('recoverPassword', token);
-            history.replace({
-                pathname: RouteConfig.RecoverPassword.path,
-            });
-        };
-
         checkToken();
     }, []);
 
-    const history = useHistory();
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     };
@@ -31,6 +21,6 @@ export const useAuth = () => {
     const users = new UserService(userClient);
 
     async function checkToken() {
-        return await users.checkToken(localStorage.getItem('recoverPassword'));
+        return await users.checkToken(query.get('token'));
     };
 };
