d7d3a1579a4519652912c14aaba20e4887a0dfa0
vladdezhniuk <vladdezhniuk@gmail.com>
2021-09-22 18:09:51 +0300

    roadmap adaptivity

--- web/landing/src/app/components/WelcomePage/Roadmap/RoadmapPoint/index.tsx
+++ web/landing/src/app/components/WelcomePage/Roadmap/RoadmapPoint/index.tsx
@@ -16,34 +16,34 @@ export const RoadmapPoint: React.FC<{
 }> = ({
     item
 }) => {
-        useEffect(() => {
-            Aos.init({
-                duration: 1000,
-            });
-        }, []);
+    useEffect(() => {
+        Aos.init({
+            duration: 1000,
+        });
+    }, []);
 
-        return (
-            <div
-                className="roadmap-point"
-                data-aos={item.id % 2 === 0 ? 'zoom-in-left-custom' : 'zoom-in-right-custom'}
-                data-aos-delay={200 * item.id}
-                data-aos-duration="600"
-                data-aos-easing="ease-in-out-cubic"
-            >
-                <p className="roadmap-point__date">
-                    {item.date}
-                </p>
-                <ul className="roadmap-point__list">
-                    {item.points.map((point, index) => (
-                        <li
-                            className="roadmap-point__item"
-                            key={index}
-                        >
-                            {point}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-    };
+    return (
+        <div
+            className="roadmap-point"
+            data-aos={item.id % 2 === 0 ? 'zoom-in-left-custom' : 'zoom-in-right-custom'}
+            data-aos-delay={200 * item.id}
+            data-aos-duration="600"
+            data-aos-easing="ease-in-out-cubic"
+        >
+            <p className="roadmap-point__date">
+                {item.date}
+            </p>
+            <ul className="roadmap-point__list">
+                {item.points.map((point, index) => (
+                    <li
+                        className="roadmap-point__item"
+                        key={index}
+                    >
+                        {point}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
