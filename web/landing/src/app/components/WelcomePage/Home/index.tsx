96011c42eff7f74a136562fc0f5a15239d89a861
PTaras <potsikaylot@gmail.com>
2021-09-26 13:14:52 +0300

    add aos animation

--- web/landing/src/app/components/WelcomePage/Home/index.tsx
+++ web/landing/src/app/components/WelcomePage/Home/index.tsx
@@ -4,13 +4,18 @@ import React from 'react';
 import PlayerIllustration from '@static/images/home/Player-Illustration.png';
 
 import './index.scss';
+import { ScrollTop } from '../ScrollTop';
 
 export const Home: React.FC = () => {
 
     return (
         <section className="ultimatedivision-home" id="home">
             <div className="wrapper">
-                <div className="ultimatedivision-home__text-left">
+                <div className="ultimatedivision-home__text-left" 
+                    data-aos="fade-right"
+                    data-aos-duration="600"
+                    data-aos-easing="ease-in-out-cubic"
+                >
                     <span className="value">10 000</span>
                     <span className="title">Unique Collectible Player Cards.</span>
                     <span className="description">
@@ -18,9 +23,14 @@ export const Home: React.FC = () => {
                         Build your club in the metaverse.
                     </span>
                 </div>
-                <picture className="ultimatedivision-home__player-image">
+                <picture className="ultimatedivision-home__player-image"
+                    data-aos="fade-left"
+                    data-aos-duration="600"
+                    data-aos-easing="ease-in-out-cubic"
+                >
                     <img src={PlayerIllustration} alt="Player Illustration"></img>
                 </picture>
+                <ScrollTop />
             </div>
         </section>
     );
