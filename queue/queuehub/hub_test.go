f8a9f50a90d0676bd0aa79c0eb2d505bcd85f3c5
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-09-28 10:44:18 +0300

    queue/queuehub: added test

---
+++ queue/queuehub/hub_test.go
@@ -0,0 +1,270 @@
+// Copyright (C) 2021 Creditor Corp. Group.
+// See LICENSE for copying information.
+
+package queuehub
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"ultimatedivision/users"
+)
+
+type UserDB struct {
+	Email    string `json:"email"`
+	NickName string `json:"nickName"`
+}
+
+func TestQueueHub(t *testing.T) {
+	userLogin1 := map[string]string{
+		"email":    "dmytroyakimuk@gmail.com",
+		"password": "1212-Dima",
+	}
+	userLogin2 := map[string]string{
+		"email":    "dmytroyakimuk@gmail.com2",
+		"password": "1212-Dima2",
+	}
+
+	var cookie1, cookie2 string
+	var userDB1, userDB2 UserDB
+
+	t.Run("login user1", func(t *testing.T) {
+		json_data, err := json.Marshal(userLogin1)
+		require.NoError(t, err)
+
+		resp, err := http.Post("http://localhost:8088/api/v0/auth/login", "application/json", bytes.NewBuffer(json_data))
+		require.NoError(t, err)
+
+		if resp.Status != "200 OK" {
+			var res map[string]interface{}
+			json.NewDecoder(resp.Body).Decode(&res)
+			fmt.Println(res)
+			require.NoError(t, fmt.Errorf("status not 200 OK"))
+		} else {
+			cookie1 = resp.Cookies()[0].Value
+		}
+	})
+
+	t.Run("get profile user1", func(t *testing.T) {
+		urlObj, err := url.Parse("http://localhost:8088/")
+		require.NoError(t, err)
+		cookie := &http.Cookie{
+			Name:     "ultimatedivision_console",
+			Value:    cookie1,
+			Path:     "/",
+			Domain:   "localhost",
+			Expires:  time.Now().Add(time.Hour * 24),
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+			Secure:   false,
+		}
+		jar, err := cookiejar.New(&cookiejar.Options{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		jar.SetCookies(urlObj, []*http.Cookie{cookie})
+		client := &http.Client{
+			Jar: jar,
+		}
+
+		resp, err := client.Get("http://localhost:8088/api/v0/profile")
+		require.NoError(t, err)
+
+		if resp.Status != "200 OK" {
+			var res map[string]interface{}
+			json.NewDecoder(resp.Body).Decode(&res)
+			fmt.Println(res)
+			require.NoError(t, fmt.Errorf("status not 200 OK"))
+		} else {
+			json.NewDecoder(resp.Body).Decode(&userDB1)
+		}
+	})
+
+	go t.Run("test queue1", func(t *testing.T) {
+		urlObj, err := url.Parse("http://localhost:8088/")
+		require.NoError(t, err)
+
+		cookie := &http.Cookie{
+			Name:     "ultimatedivision_console",
+			Value:    cookie1,
+			Path:     "/",
+			Domain:   "localhost",
+			Expires:  time.Now().Add(time.Hour * 24),
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+			Secure:   false,
+		}
+
+		jar, err := cookiejar.New(&cookiejar.Options{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		jar.SetCookies(urlObj, []*http.Cookie{cookie})
+		var dialer = &websocket.Dialer{
+			HandshakeTimeout: 4500000 * time.Second,
+			Jar:              jar,
+		}
+
+		c, _, err := dialer.Dial("ws://localhost:8088/api/v0/queue", nil)
+		require.NoError(t, err)
+
+		// write to searches
+		request := Request{
+			Action: "search",
+			Value:  true,
+		}
+		err = c.WriteJSON(request)
+		require.NoError(t, err)
+		////////////////////////
+
+		// read answer
+		var message Message
+		err = c.ReadJSON(&message)
+		require.NoError(t, err)
+		fmt.Println(message)
+		assert.Equal(t, 200, message.Status)
+		/////////////////////////
+
+		// read invite
+		var user users.User
+		err = c.ReadJSON(&user)
+		require.NoError(t, err)
+		assert.Equal(t, userDB2.Email, user.Email)
+		assert.Equal(t, userDB2.NickName, user.NickName)
+		////////////////////////
+
+		// write answer invite
+		request2 := Request{
+			Action: ActionPlay,
+			Value:  true,
+		}
+		err = c.WriteJSON(request2)
+		require.NoError(t, err)
+		///////////////////////
+	})
+
+	t.Run("login user2", func(t *testing.T) {
+		json_data, err := json.Marshal(userLogin2)
+		require.NoError(t, err)
+
+		resp, err := http.Post("http://localhost:8088/api/v0/auth/login", "application/json", bytes.NewBuffer(json_data))
+
+		if resp.Status != "200 OK" {
+			var res map[string]interface{}
+			json.NewDecoder(resp.Body).Decode(&res)
+			fmt.Println(res)
+			require.NoError(t, fmt.Errorf("status not 200 OK"))
+		} else {
+			cookie2 = resp.Cookies()[0].Value
+		}
+	})
+
+	t.Run("get profile user2", func(t *testing.T) {
+		urlObj, err := url.Parse("http://localhost:8088/")
+		require.NoError(t, err)
+		cookie := &http.Cookie{
+			Name:     "ultimatedivision_console",
+			Value:    cookie2,
+			Path:     "/",
+			Domain:   "localhost",
+			Expires:  time.Now().Add(time.Hour * 24),
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+			Secure:   false,
+		}
+		jar, err := cookiejar.New(&cookiejar.Options{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		jar.SetCookies(urlObj, []*http.Cookie{cookie})
+		client := &http.Client{
+			Jar: jar,
+		}
+
+		resp, err := client.Get("http://localhost:8088/api/v0/profile")
+		require.NoError(t, err)
+
+		if resp.Status != "200 OK" {
+			var res map[string]interface{}
+			json.NewDecoder(resp.Body).Decode(&res)
+			fmt.Println(res)
+			require.NoError(t, fmt.Errorf("status not 200 OK"))
+		} else {
+			json.NewDecoder(resp.Body).Decode(&userDB2)
+		}
+	})
+
+	t.Run("test queue2", func(t *testing.T) {
+		urlObj, err := url.Parse("http://localhost:8088/")
+		require.NoError(t, err)
+
+		cookie := &http.Cookie{
+			Name:     "ultimatedivision_console",
+			Value:    cookie2,
+			Path:     "/",
+			Domain:   "localhost",
+			Expires:  time.Now().Add(time.Hour * 24),
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+			Secure:   false,
+		}
+
+		jar, err := cookiejar.New(&cookiejar.Options{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		jar.SetCookies(urlObj, []*http.Cookie{cookie})
+		var dialer = &websocket.Dialer{
+			HandshakeTimeout: 4500000 * time.Second,
+			Jar:              jar,
+		}
+
+		c, _, err := dialer.Dial("ws://localhost:8088/api/v0/queue", nil)
+		require.NoError(t, err)
+
+		// write to searches
+		request := Request{
+			Action: "search",
+			Value:  true,
+		}
+		err = c.WriteJSON(request)
+		require.NoError(t, err)
+		////////////////////////
+
+		// read answer
+		var message Message
+		err = c.ReadJSON(&message)
+		require.NoError(t, err)
+		fmt.Println(message)
+		assert.Equal(t, 200, message.Status)
+		/////////////////////////
+
+		// read invite
+		var user users.User
+		err = c.ReadJSON(&user)
+		require.NoError(t, err)
+		assert.Equal(t, userDB1.Email, user.Email)
+		assert.Equal(t, userDB1.NickName, user.NickName)
+		////////////////////////
+
+		// write answer invite
+		request2 := Request{
+			Action: ActionPlay,
+			Value:  true,
+		}
+		err = c.WriteJSON(request2)
+		require.NoError(t, err)
+		/////
+	})
+}
