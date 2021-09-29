7cc3c74856f6a06ab1790d53c71929228effb808
vladdezhniuk <vladdezhniuk@gmail.com>
2021-09-29 17:22:27 +0300

    metamask call

--- web/landing/src/app/components/common/MintButton/index.tsx
+++ web/landing/src/app/components/common/MintButton/index.tsx
@@ -1,6 +1,8 @@
+//@ts-nocheck
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
-import React from 'react';
+import MetaMaskOnboarding from '@metamask/onboarding';
+import React, { useEffect } from 'react';
 
 import './index.scss';
 
@@ -10,12 +12,31 @@ type MintButtonProps = {
 
 export const MintButton: React.FC<MintButtonProps> = (
     { text }) => {
+    const [accounts, setAccounts] = React.useState([]);
+    const onboarding = React.useRef<MetaMaskOnboarding>();
+
+    useEffect(() => {
+        if (!onboarding.current) {
+            onboarding.current = new MetaMaskOnboarding();
+        }
+    }, []);
+
+    const handleClick = () => {
+        if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+            window.ethereum
+                .request({ method: 'eth_requestAccounts' })
+                .then((newAccounts) => setAccounts(newAccounts));
+        } else {
+            onboarding.current.startOnboarding();
+        }
+    };
 
     return (
-        <button className="ultimatedivision-mint-btn" 
+        <button className="ultimatedivision-mint-btn"
             data-aos="fade-right"
             data-aos-duration="600"
             data-aos-easing="ease-in-out-cubic"
+            onClick={handleClick}
         >
             <span className="ultimatedivision-mint-btn__text">{text}</span>
         </button>
