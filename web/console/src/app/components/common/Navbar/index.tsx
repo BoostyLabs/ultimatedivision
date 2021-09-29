490a2133f11653101316f1dde038bdb0368f1906
TarasPotsikaylo <potsikaylot@gmail.com>
2021-09-29 14:55:15 +0300

    layout response for mobile

--- web/console/src/app/components/common/Navbar/index.tsx
+++ web/console/src/app/components/common/Navbar/index.tsx
@@ -1,56 +1,73 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
-import { NavLink } from 'react-router-dom';
-import { RouteConfig } from '@/app/router';
-import ultimate from '@static/img/Navbar/ultimate.svg';
-import './index.scss';
-export const Navbar: React.FC = () =>
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RouteConfig } from "@/app/router";
+import { DropDownIcon } from "@/app/static/img/Navbar";
+import ultimate from "@static/img/Navbar/ultimate.svg";
+
+import "./index.scss";
+
+export const Navbar: React.FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+  const dropdownClass = isDropdownOpen ? "-active" : "";
+
+  return (
     <div className="ultimatedivision-navbar">
-        <img
-            className="ultimatedivision-navbar__logo"
-            src={ultimate}
-            alt="UltimateDivision logo"
-        />
-        <ul className="ultimatedivision-navbar__list">
-            <li className="ultimatedivision-navbar__item">
-                <NavLink
-                    to={RouteConfig.Summary.path}
-                    className="ultimatedivision-navbar__item__active"
-                >
-                    HOME
-                </NavLink>
-            </li>
-            <li className="ultimatedivision-navbar__item">
-                <NavLink
-                    to={RouteConfig.Store.path}
-                    className="ultimatedivision-navbar__item__active"
-                >
-                    Store
-                </NavLink>
-            </li>
-            <li className="ultimatedivision-navbar__item">
-                <NavLink
-                    to={RouteConfig.MarketPlace.path}
-                    className="ultimatedivision-navbar__item__active"
-                >
-                    MARKETPLACE
-                </NavLink>
-            </li>
-            <li className="ultimatedivision-navbar__item">
-                <NavLink
-                    to={RouteConfig.Club.path}
-                    className="ultimatedivision-navbar__item__active"
-                >
-                    CLUB
-                </NavLink>
-            </li>
-            <li className="ultimatedivision-navbar__item">
-                <NavLink
-                    to={RouteConfig.FootballField.path}
-                    className="ultimatedivision-navbar__item__active"
-                >
-                    FIELD
-                </NavLink>
-            </li>
-        </ul >
-    </div >;
+      <img
+        className="ultimatedivision-navbar__logo"
+        src={ultimate}
+        alt="UltimateDivision logo"
+      />
+      <ul className={`ultimatedivision-navbar__list${dropdownClass}`}>
+        <li className="ultimatedivision-navbar__item">
+          <NavLink
+            to={RouteConfig.Summary.path}
+            className="ultimatedivision-navbar__item__active"
+          >
+            HOME
+          </NavLink>
+        </li>
+        <li className="ultimatedivision-navbar__item">
+          <NavLink
+            to={RouteConfig.Store.path}
+            className="ultimatedivision-navbar__item__active"
+          >
+            Store
+          </NavLink>
+        </li>
+        <li className="ultimatedivision-navbar__item">
+          <NavLink
+            to={RouteConfig.MarketPlace.path}
+            className="ultimatedivision-navbar__item__active"
+          >
+            MARKETPLACE
+          </NavLink>
+        </li>
+        <li className="ultimatedivision-navbar__item">
+          <NavLink
+            to={RouteConfig.Club.path}
+            className="ultimatedivision-navbar__item__active"
+          >
+            CLUB
+          </NavLink>
+        </li>
+        <li className="ultimatedivision-navbar__item">
+          <NavLink
+            to={RouteConfig.FootballField.path}
+            className="ultimatedivision-navbar__item__active"
+          >
+            FIELD
+          </NavLink>
+        </li>
+      </ul>
+      <div
+        className="ultimatedivision-navbar__dropdown"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+      >
+        <DropDownIcon />
+      </div>
+    </div>
+  );
+};
---
+++ web/console/src/app/static/img/Navbar/index.tsx
@@ -0,0 +1,16 @@
+export function DropDownIcon() {
+  return (
+    <svg
+      className="dropdown"
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="28" height="6" fill="white" />
+      <rect y="11" width="28" height="6" fill="white" />
+      <rect y="22" width="28" height="6" fill="white" />
+    </svg>
+  );
+}
