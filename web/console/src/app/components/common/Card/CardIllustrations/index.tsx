a17ce9d39213f5700ffe3f28270932e41cff5774
TarasPotsikaylo <potsikaylot@gmail.com>
2021-09-30 12:32:52 +0300

    layout desktop width for response

--- web/console/src/app/components/common/Card/CardIllustrations/index.tsx
+++ web/console/src/app/components/common/Card/CardIllustrations/index.tsx
@@ -1,19 +1,27 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { FootballerCardIllustrationsDiagramsArea } from '@/app/components/common/Card/CardIllustrationsDiagramsArea';
-import { FootballerCardIllustrationsRadar } from '@/app/components/common/Card/CardIllustrationsRadar';
+import { FootballerCardIllustrationsDiagramsArea } from "@/app/components/common/Card/CardIllustrationsDiagramsArea";
+import { FootballerCardIllustrationsRadar } from "@/app/components/common/Card/CardIllustrationsRadar";
 
-import { Card } from '@/card';
-import { PlayerCard } from '@components/common/PlayerCard';
+import { Card } from "@/card";
+import { PlayerCard } from "@components/common/PlayerCard";
 
-import './index.scss';
+import "./index.scss";
 
-export const FootballerCardIllustrations: React.FC<{ card: Card }> = ({ card }) =>
-    <div className="footballer-card-illustrations">
-        <div className="footballer-card-illustrations__card">
-            <PlayerCard card={card} parentClassName="footballer-card-illustrations__card" />
-        </div>
-        <FootballerCardIllustrationsRadar card={card} />
-        <FootballerCardIllustrationsDiagramsArea card={card} />
-    </div>;
+export const FootballerCardIllustrations: React.FC<{ card: Card }> = ({
+  card,
+}) => (
+  <div className="footballer-card-illustrations">
+    <div className="footballer-card-illustrations__card">
+      <PlayerCard
+        card={card}
+        parentClassName="footballer-card-illustrations__card"
+      />
+    </div>
+    <div className="footballer-card-illustrations__divider"></div>
+    <FootballerCardIllustrationsRadar card={card} />
+    <div className="footballer-card-illustrations__divider"></div>
+    <FootballerCardIllustrationsDiagramsArea card={card} />
+  </div>
+);
--- web/console/src/app/router/index.tsx
+++ web/console/src/app/router/index.tsx
@@ -1,168 +1,173 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { lazy } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { lazy } from "react";
+import { Route, Switch } from "react-router-dom";
 
-const MarketPlace = lazy(() => import('@/app/views/MarketPlacePage'));
-const Club = lazy(() => import('@/app/views/ClubPage'));
-const Card = lazy(() => import('@/app/views/CardPage'));
-const Lot = lazy(() => import('@/app/views/LotPage'));
-const FootballField = lazy(() => import('@/app/views/FootballFieldPage'));
-const WhitePaper = lazy(() => import('@/app/views/WhitePaperPage'));
-const Tokenomics = lazy(() => import('@/app/views/TokenomicsPage'));
-const Store = lazy(() => import('@/app/views/StorePage'));
+const MarketPlace = lazy(() => import("@/app/views/MarketPlacePage"));
+const Club = lazy(() => import("@/app/views/ClubPage"));
+const Card = lazy(() => import("@/app/views/CardPage"));
+const Lot = lazy(() => import("@/app/views/LotPage"));
+const FootballField = lazy(() => import("@/app/views/FootballFieldPage"));
+const WhitePaper = lazy(() => import("@/app/views/WhitePaperPage"));
+const Tokenomics = lazy(() => import("@/app/views/TokenomicsPage"));
+const Store = lazy(() => import("@/app/views/StorePage"));
 
-import Summary from '@components/WhitePaper/Summary';
-import GameMechanics from '@components/WhitePaper/GameMechanics';
-import PayToEarnEconomy from '@components/WhitePaper/PayToEarnEconomy';
-import Technology from '@components/WhitePaper/Technology';
-import Fund from '@components/Tokenomics/Fund';
-import PlayToEarn from '@components/Tokenomics/PlayToEarn';
-import Spending from '@components/Tokenomics/Spending';
-import Staking from '@components/Tokenomics/Staking';
+import Summary from "@components/WhitePaper/Summary";
+import GameMechanics from "@components/WhitePaper/GameMechanics";
+import PayToEarnEconomy from "@components/WhitePaper/PayToEarnEconomy";
+import Technology from "@components/WhitePaper/Technology";
+import Fund from "@components/Tokenomics/Fund";
+import PlayToEarn from "@components/Tokenomics/PlayToEarn";
+import Spending from "@components/Tokenomics/Spending";
+import Staking from "@components/Tokenomics/Staking";
 
 /** Route base config implementation */
 export class ComponentRoutes {
-    /** data route config*/
-    constructor(
-        public path: string,
-        public component: React.FC<any>,
-        public exact: boolean,
-        public children?: ComponentRoutes[]
-    ) { }
-    /** Method for creating child subroutes path */
-    public with(child: ComponentRoutes, parrent: ComponentRoutes): ComponentRoutes {
-        child.path = `${parrent.path}/${child.path}`;
+  /** data route config*/
+  constructor(
+    public path: string,
+    public component: React.FC<any>,
+    public exact: boolean,
+    public children?: ComponentRoutes[]
+  ) {}
+  /** Method for creating child subroutes path */
+  public with(
+    child: ComponentRoutes,
+    parrent: ComponentRoutes
+  ): ComponentRoutes {
+    child.path = `${parrent.path}/${child.path}`;
 
-        return this;
-    }
-    /** Call with method for each child */
-    public addChildren(children: ComponentRoutes[]): ComponentRoutes {
-        this.children = children.map(item => item.with(item, this));
+    return this;
+  }
+  /** Call with method for each child */
+  public addChildren(children: ComponentRoutes[]): ComponentRoutes {
+    this.children = children.map((item) => item.with(item, this));
 
-        return this;
-    }
-};
+    return this;
+  }
+}
 
 /** Route config implementation */
 export class RouteConfig {
-    public static MarketPlace: ComponentRoutes = new ComponentRoutes(
-        '/marketplace',
-        MarketPlace,
-        true,
-    );
-    public static Lot: ComponentRoutes = new ComponentRoutes(
-        '/lot/:id',
-        Lot,
-        true,
-    );
-    public static Card: ComponentRoutes = new ComponentRoutes(
-        '/card/:id',
-        Card,
-        false,
-    );
-    public static FootballField: ComponentRoutes = new ComponentRoutes(
-        '/field',
-        FootballField,
-        true,
-    );
-    public static Store: ComponentRoutes = new ComponentRoutes(
-        '/store',
-        Store,
-        true,
-    );
-    public static Club: ComponentRoutes = new ComponentRoutes(
-        '/club',
-        Club,
-        true,
-    );
-    public static Whitepaper: ComponentRoutes = new ComponentRoutes(
-        '/whitepaper',
-        WhitePaper,
-        false
-    );
-    public static Tokenomics: ComponentRoutes = new ComponentRoutes(
-        '/tokenomics',
-        Tokenomics,
-        false
-    );
-    public static Summary: ComponentRoutes = new ComponentRoutes(
-        'summary',
-        Summary,
-        true
-    );
-    public static GameMechanick: ComponentRoutes = new ComponentRoutes(
-        'game-mechanicks',
-        GameMechanics,
-        true
-    );
-    public static PayToEarnEconomy: ComponentRoutes = new ComponentRoutes(
-        'pay-to-earn-and-economy',
-        PayToEarnEconomy,
-        true
-    );
-    public static Technology: ComponentRoutes = new ComponentRoutes(
-        'technology',
-        Technology,
-        true
-    );
-    public static Spending: ComponentRoutes = new ComponentRoutes(
-        'udt-spending',
-        Spending,
-        true
-    );
-    public static PayToEarn: ComponentRoutes = new ComponentRoutes(
-        'pay-to-earn',
-        PlayToEarn,
-        true
-    );
-    public static Staking: ComponentRoutes = new ComponentRoutes(
-        'staking',
-        Staking,
-        true
-    );
-    public static Fund: ComponentRoutes = new ComponentRoutes(
-        'ud-dao-fund',
-        Fund,
-        true
-    );
-    public static Default: ComponentRoutes = new ComponentRoutes(
-        '/',
-        MarketPlace,
-        true,
-    );
-    public static routes: ComponentRoutes[] = [
-        RouteConfig.Default,
-        RouteConfig.FootballField,
-        RouteConfig.MarketPlace,
-        RouteConfig.Club,
-        RouteConfig.Card,
-        RouteConfig.Lot,
-        RouteConfig.Store,
-        RouteConfig.Whitepaper.addChildren([
-            RouteConfig.Summary,
-            RouteConfig.GameMechanick,
-            RouteConfig.PayToEarnEconomy,
-            RouteConfig.Technology,
-        ]),
-        RouteConfig.Tokenomics.addChildren([
-            RouteConfig.Spending,
-            RouteConfig.PayToEarn,
-            RouteConfig.Staking,
-            RouteConfig.Fund,
-        ]),
-    ];
-};
+  public static MarketPlace: ComponentRoutes = new ComponentRoutes(
+    "/marketplace",
+    MarketPlace,
+    true
+  );
+  public static Lot: ComponentRoutes = new ComponentRoutes(
+    "/lot/:id",
+    Lot,
+    true
+  );
+  public static Card: ComponentRoutes = new ComponentRoutes(
+    // "/card/:id",
+    "/card/",
+    Card,
+    false
+  );
+  public static FootballField: ComponentRoutes = new ComponentRoutes(
+    "/field",
+    FootballField,
+    true
+  );
+  public static Store: ComponentRoutes = new ComponentRoutes(
+    "/store",
+    Store,
+    true
+  );
+  public static Club: ComponentRoutes = new ComponentRoutes(
+    "/club",
+    Club,
+    true
+  );
+  public static Whitepaper: ComponentRoutes = new ComponentRoutes(
+    "/whitepaper",
+    WhitePaper,
+    false
+  );
+  public static Tokenomics: ComponentRoutes = new ComponentRoutes(
+    "/tokenomics",
+    Tokenomics,
+    false
+  );
+  public static Summary: ComponentRoutes = new ComponentRoutes(
+    "summary",
+    Summary,
+    true
+  );
+  public static GameMechanick: ComponentRoutes = new ComponentRoutes(
+    "game-mechanicks",
+    GameMechanics,
+    true
+  );
+  public static PayToEarnEconomy: ComponentRoutes = new ComponentRoutes(
+    "pay-to-earn-and-economy",
+    PayToEarnEconomy,
+    true
+  );
+  public static Technology: ComponentRoutes = new ComponentRoutes(
+    "technology",
+    Technology,
+    true
+  );
+  public static Spending: ComponentRoutes = new ComponentRoutes(
+    "udt-spending",
+    Spending,
+    true
+  );
+  public static PayToEarn: ComponentRoutes = new ComponentRoutes(
+    "pay-to-earn",
+    PlayToEarn,
+    true
+  );
+  public static Staking: ComponentRoutes = new ComponentRoutes(
+    "staking",
+    Staking,
+    true
+  );
+  public static Fund: ComponentRoutes = new ComponentRoutes(
+    "ud-dao-fund",
+    Fund,
+    true
+  );
+  public static Default: ComponentRoutes = new ComponentRoutes(
+    "/",
+    MarketPlace,
+    true
+  );
+  public static routes: ComponentRoutes[] = [
+    RouteConfig.Default,
+    RouteConfig.FootballField,
+    RouteConfig.MarketPlace,
+    RouteConfig.Club,
+    RouteConfig.Card,
+    RouteConfig.Lot,
+    RouteConfig.Store,
+    RouteConfig.Whitepaper.addChildren([
+      RouteConfig.Summary,
+      RouteConfig.GameMechanick,
+      RouteConfig.PayToEarnEconomy,
+      RouteConfig.Technology,
+    ]),
+    RouteConfig.Tokenomics.addChildren([
+      RouteConfig.Spending,
+      RouteConfig.PayToEarn,
+      RouteConfig.Staking,
+      RouteConfig.Fund,
+    ]),
+  ];
+}
 
-export const Routes = () =>
-    <Switch>
-        {RouteConfig.routes.map((route, index) =>
-            <Route
-                key={index}
-                path={route.path}
-                component={route.component}
-                exact={route.exact}
-            />
-        )}
-    </Switch>;
+export const Routes = () => (
+  <Switch>
+    {RouteConfig.routes.map((route, index) => (
+      <Route
+        key={index}
+        path={route.path}
+        component={route.component}
+        exact={route.exact}
+      />
+    ))}
+  </Switch>
+);
