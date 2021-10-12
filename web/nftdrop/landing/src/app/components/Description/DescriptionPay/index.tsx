a3fab17a848d0b0bd490bd1151b43fa1f055d29e
TarasPotsikaylo <potsikaylot@gmail.com>
2021-10-12 20:33:51 +0300

    implement animation

--- web/nftdrop/landing/src/app/components/Description/DescriptionPay/index.tsx
+++ web/nftdrop/landing/src/app/components/Description/DescriptionPay/index.tsx
@@ -1,27 +1,53 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import radar from '@static/images/description/radar.svg';
+import { useEffect } from "react";
+import lottie from "lottie-web";
+import playToEarn from "@static/images/description/playToEarn/data.json";
+import radar from "@static/images/description/radar.svg";
+import images_0 from "@static/images/description/playToEarn/images/img_0.png";
+import images_1 from "@static/images/description/playToEarn/images/img_1.png";
+import images_2 from "@static/images/description/playToEarn/images/img_2.png";
+import images_3 from "@static/images/description/playToEarn/images/img_3.png";
+import images_4 from "@static/images/description/playToEarn/images/img_4.png";
 
-import './index.scss';
+import "./index.scss";
 
 export const DescriptionPay = () => {
+    const loadedImagesData = JSON.stringify(playToEarn);
+    const parsedImagesData = JSON.parse(loadedImagesData);
+    const images: string[] = [images_0, images_1, images_2, images_3, images_4];
+
+    const changeImagesPath = parsedImagesData.assets.map(
+        //@ts-ignore
+        (img: string, i: number) => (img.p = images[i])
+    );
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            //@ts-ignore
+            container: document.querySelector(".description-pay__radar"),
+            animationData: parsedImagesData,
+        });
+    }, []);
 
     return (
         <div className="description-pay">
-            <img
+            {/* <img
                 className="description-pay__radar"
                 src={radar}
                 alt="radar diagram"
                 loading="lazy"
-            />
+            /> */}
+
+            <div className="description-pay__radar"></div>
             <div className="description-pay__text-area">
                 <h2 className="description-pay__title">Play-to-Earn</h2>
                 <p className="description-pay__text">
                     Club Owners who hold a Founder Collection NFT will be
                     awarded the in-game title of UD Founder. The UD Founders
-                    will receive exclusive airdrops and will start
-                    the game in UD’s top division.
+                    will receive exclusive airdrops and will start the game in
+                    UD’s top division.
                 </p>
             </div>
         </div>
--- web/nftdrop/landing/src/app/components/LaunchRoadmap/Card/index.tsx
+++ web/nftdrop/landing/src/app/components/LaunchRoadmap/Card/index.tsx
@@ -1,44 +1,88 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
+import React, { useEffect } from "react";
+import lottie from "lottie-web";
+import box from "@static/images/launchRoadmap/box1.svg";
+import roadmapDiagram from "@static/images/launchRoadmap/animated-diagram/data.json";
+import images_0 from "@static/images/launchRoadmap/animated-diagram/images/img_0.png";
+import images_1 from "@static/images/launchRoadmap/animated-diagram/images/img_1.png";
+import images_2 from "@static/images/launchRoadmap/animated-diagram/images/img_2.png";
+import images_3 from "@static/images/launchRoadmap/animated-diagram/images/img_3.png";
+import images_4 from "@static/images/launchRoadmap/animated-diagram/images/img_4.png";
 
-import box from '@static/images/launchRoadmap/box1.svg';
-
-import './index.scss';
+import "./index.scss";
 
 export const Card: React.FC<{
     card: {
-        title: string,
-        subTitle: string,
-        description: string,
-        image: string,
-    }
+        id: string;
+        title: string;
+        subTitle: string;
+        description: string;
+        image: string;
+        percent: string;
+    };
 }> = ({ card }) => {
+    const loadedImagesData = JSON.stringify(roadmapDiagram);
+    const parsedImagesData = JSON.parse(loadedImagesData);
+    const images: string[] = [images_0, images_1, images_2, images_3, images_4];
+
+    // const percents: string[] = ["15%", "50%", "80%", "100%"];
+
+    const changeImagesPath = parsedImagesData.assets.map(
+        //@ts-ignore
+        (img: string, i: number) => {
+            //@ts-ignore
+            img.p = images[i];
+            //@ts-ignore
+            img[5] = card.percent;
+            //@ts-ignore
+            return;
+        }
+    );
+
+    parsedImagesData.layers.map(
+        //@ts-ignore
+        (p: string, i: number) => {
+            //@ts-ignore
+            p[0] = card.percent;
+            //@ts-ignore
+            return;
+        }
+    );
+
+    // parsedImagesData.assets[5].nm = card.percent;
+    // parsedImagesData.layers[0].nm = card.percent;
+    // console.log(parsedImagesData);
+    // console.log(card.percent);
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            //@ts-ignore
+            container: document.querySelector(`.card__image-${card.id}`),
+            animationData: parsedImagesData,
+        });
+    }, []);
 
     return (
         <div className="card">
             <div className="card__text-area">
-                <h1 className="card__title">
-                    {card.title}
-                </h1>
-                <p className="card__description">
-                    {card.description}
-                </p>
+                <h1 className="card__title">{card.title}</h1>
+                <p className="card__description">{card.description}</p>
                 <div className="card__box">
                     <img
                         className="card__box__present"
                         src={box}
                         alt="utlimate box"
                     />
-                    <p className="card__box__subtitle">
-                        {card.subTitle}
-                    </p>
+                    <p className="card__box__subtitle">{card.subTitle}</p>
                 </div>
             </div>
-            <img
+            {/* <img
                 src={card.image}
                 alt="diagram"
                 className="card__image"
-            />
+            /> */}
+            <div className={`card__image-${card.id}`}></div>
         </div>
     );
 };
