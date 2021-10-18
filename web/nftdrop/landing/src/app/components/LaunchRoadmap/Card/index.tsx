c64e38e338e12153ab045fa1735b45c3640807ba
TarasPotsikaylo <potsikaylot@gmail.com>
2021-10-18 16:46:20 +0300

    delete unuse images

--- web/nftdrop/landing/src/app/components/LaunchRoadmap/Card/index.tsx
+++ web/nftdrop/landing/src/app/components/LaunchRoadmap/Card/index.tsx
@@ -1,18 +1,12 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
-import React from 'react';
+import React from "react";
 
-import { AnimationImage } from '@components/common/AnimationImage';
+import { AnimationImage } from "@components/common/AnimationImage";
 
-import box from '@static/images/launchRoadmap/box.svg';
+import box from "@static/images/launchRoadmap/box.svg";
 
-import animationImage_0 from '@static/images/launchRoadmap/animated-diagram/images/img_0.png';
-import animationImage_1 from '@static/images/launchRoadmap/animated-diagram/images/img_1.png';
-import animationImage_2 from '@static/images/launchRoadmap/animated-diagram/images/img_2.png';
-import animationImage_3 from '@static/images/launchRoadmap/animated-diagram/images/img_3.png';
-import animationImage_4 from '@static/images/launchRoadmap/animated-diagram/images/img_4.png';
-
-import './index.scss';
+import "./index.scss";
 
 export const Card: React.FC<{
     card: {
@@ -23,14 +17,6 @@ export const Card: React.FC<{
         animation: any;
     };
 }> = ({ card }) => {
-    const animationImages: string[] = [
-        animationImage_0,
-        animationImage_1,
-        animationImage_2,
-        animationImage_3,
-        animationImage_4,
-    ];
-
     return (
         <div className="card">
             <div className="card__text-area">
@@ -51,7 +37,7 @@ export const Card: React.FC<{
                 heightTo={-200}
                 loop={true}
                 animationData={card.animation}
-                animationImages={animationImages}
+                animationImages={[]}
             />
         </div>
     );
