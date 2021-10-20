c434de6117d05fb303da6074ace67dde20a53b72
TarasPotsikaylo <potsikaylot@gmail.com>
2021-10-20 11:00:30 +0300

    home animation part1

--- web/nftdrop/landing/src/app/components/Home/index.tsx
+++ web/nftdrop/landing/src/app/components/Home/index.tsx
@@ -1,35 +1,37 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import React from 'react';
+import React from "react";
 
-import { ScrollTop } from '../ScrollTop';
+import { ScrollTop } from "../ScrollTop";
+import { AnimationImage } from "@components/common/AnimationImage";
 
-import webkitFootballer from '@static/images/home/footballer.png';
-import webkitFootballerTablet from '@static/images/home/footballerTablet.png';
-import webkitFootballerMobile from '@static/images/home/footballerMobile.png';
-import footballer from '@static/images/home/footballer.webp';
-import footballerTablet from '@static/images/home/footballerTablet.webp';
-import footballerMobile from '@static/images/home/footballerMobile.webp';
+import footballerAnimation from "@static/images/home/animation-home/data.json";
 
-import discord from '@static/images/home/discord.svg';
-import twitter from '@static/images/home/twitter.svg';
+import webkitFootballer from "@static/images/home/footballer.png";
+import webkitFootballerTablet from "@static/images/home/footballerTablet.png";
+import webkitFootballerMobile from "@static/images/home/footballerMobile.png";
+import footballer from "@static/images/home/footballer.webp";
+import footballerTablet from "@static/images/home/footballerTablet.webp";
+import footballerMobile from "@static/images/home/footballerMobile.webp";
 
-import './index.scss';
+import discord from "@static/images/home/discord.svg";
+import twitter from "@static/images/home/twitter.svg";
+
+import "./index.scss";
 
 export const Home: React.FC = () => {
-
     return (
         <section className="home" id="home">
             <div className="home__wrapper">
-                <div className="home__text-area"
-
-                >
+                <div className="home__text-area">
                     <h1 className="home__value">10 000</h1>
-                    <h3 className="home__title">Unique Collectible Player Cards.</h3>
+                    <h3 className="home__title">
+                        Unique Collectible Player Cards.
+                    </h3>
                     <p className="home__description">
-                        Get one to become UD founder and join the Play-to-Earn game.
-                        Build your club in the metaverse.
+                        Get one to become UD founder and join the Play-to-Earn
+                        game. Build your club in the metaverse.
                     </p>
                     <div className="home__buttons-wrapper">
                         <a
@@ -61,7 +63,15 @@ export const Home: React.FC = () => {
                         </a>
                     </div>
                 </div>
-                <picture>
+                <AnimationImage
+                    className={"home__player-image"}
+                    heightFrom={4000}
+                    heightTo={-500}
+                    loop={true}
+                    animationData={footballerAnimation}
+                    animationImages={[]}
+                />
+                {/* <picture>
                     <source
                         media="(max-width: 500px)"
                         srcSet={footballerMobile} type="image/webp"
@@ -88,11 +98,11 @@ export const Home: React.FC = () => {
                         src={webkitFootballer}
                         alt="Player Illustration"
                     />
-                </picture>
+                </picture> */}
                 <div className="home__mobile-wrapper">
                     <div className="home__description-mobile">
-                        Get one to become UD founder and join the Play-to-Earn game.
-                        Build your club in the metaverse.
+                        Get one to become UD founder and join the Play-to-Earn
+                        game. Build your club in the metaverse.
                         <div className="home__buttons-wrapper__mobile">
                             <a
                                 className="home__discord"
@@ -126,6 +136,6 @@ export const Home: React.FC = () => {
                 </div>
                 <ScrollTop />
             </div>
-        </section >
+        </section>
     );
 };
