4788f957c5370404cf15644c719deb362a7f6146
TarasPotsikaylo <potsikaylot@gmail.com>
2021-10-22 10:44:55 +0300

    fix conflict

--- web/nftdrop/landing/src/app/ethers/service.ts
+++ web/nftdrop/landing/src/app/ethers/service.ts
@@ -27,39 +27,49 @@ export class Service {
     /** Get last lot id. */
     public async getLastTokenId(wallet: string, abi: any[]) {
         const address = await this.getAddress(wallet);
-        const contract = await new ethers.Contract(address.smartContractAddress.nft, abi);
+        const contract = await new ethers.Contract(
+            address.smartContractAddress.nft,
+            abi
+        );
         const signer = await this.provider.getSigner();
         const connect = await contract.connect(signer);
         const totalSupply = await connect.functions.totalSupply();
 
         return parseInt(totalSupply[0]._hex, 16);
-    };
+    }
 
     /** Send smart contract transaction. */
-    public async sendTransaction(wallet: string, totalSupply: number, abi: any[]) {
+    public async sendTransaction(
+        wallet: string,
+        totalSupply: number,
+        abi: any[]
+    ) {
         const signer = await this.provider.getSigner();
         const address = await this.getAddress(wallet);
-        const contract = await new ethers.Contract(address.smartContractAddress.nftSale, abi);
+        const contract = await new ethers.Contract(
+            address.smartContractAddress.nftSale,
+            abi
+        );
         const connect = await contract.connect(signer);
         const currentPrice = await connect.functions.getCurrentPrice();
-        const data = `0xd399cc1a${buildHash((totalSupply + 1).toString(16))}${buildHash(40)}${buildHash(60)}${buildHash(address.password.slice(-2))}${address.password.slice(0, address.password.length - 2)}`;
+        const data = `0xd399cc1a${buildHash(
+            (totalSupply + 1).toString(16)
+        )}${buildHash(40)}${buildHash(60)}${buildHash(
+            address.password.slice(-2)
+        )}${address.password.slice(0, address.password.length - 2)}`;
         const gasLimit = await signer.estimateGas({
             to: address.smartContractAddress.nftSale,
             data,
             value: currentPrice[0],
-<<<<<<< HEAD
         });
-=======
-        })
->>>>>>> b567785d8bfbb9ded6b5df2d7c25bed0fa99f398
         const transaction = await signer.sendTransaction({
             to: address.smartContractAddress.nftSale,
             data,
             gasLimit,
             chainId: 3,
-            value: currentPrice[0]
+            value: currentPrice[0],
         });
-    };
+    }
 
     public async getBalance(id: string) {
         try {
@@ -68,7 +78,7 @@ export class Service {
             return ethers.utils.formatEther(balance);
         } catch (error: any) {
             /* eslint-disable-next-line */
-            console.log(error.message)
+            console.log(error.message);
         }
     }
-};
+}
