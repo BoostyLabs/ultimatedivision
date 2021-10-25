6aaa50465c8f9632eae73bb78c4903a4ecce0543
TarasPotsikaylo <potsikaylot@gmail.com>
2021-10-25 12:36:16 +0300

    change start launch date

--- web/nftdrop/landing/src/app/components/LaunchDate/index.tsx
+++ web/nftdrop/landing/src/app/components/LaunchDate/index.tsx
@@ -1,38 +1,48 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { Modal } from './Modal';
+import { Modal } from "./Modal";
 
-import ball from '@static/images/launchDate/ball.webp';
-import ballMobile from '@static/images/launchDate/ballMobile.webp';
-import webkitBall from '@static/images/launchDate/ball.png';
-import webkitBallMobile from '@static/images/launchDate/ballMobile.png';
+import ball from "@static/images/launchDate/ball.webp";
+import ballMobile from "@static/images/launchDate/ballMobile.webp";
+import webkitBall from "@static/images/launchDate/ball.png";
+import webkitBallMobile from "@static/images/launchDate/ballMobile.png";
 
-import './index.scss';
+import "./index.scss";
 
 export const LaunchDate: React.FC = () => {
-
     const [isShowModal, setIsShowModal] = useState(false);
 
     useEffect(() => {
         /** provides logic to disable scrolling */
         isShowModal
-            ? document.body.classList.add('scroll-hidden')
-            : document.body.classList.remove('scroll-hidden');
+            ? document.body.classList.add("scroll-hidden")
+            : document.body.classList.remove("scroll-hidden");
     }, [isShowModal]);
 
-    const handleModal = () => setIsShowModal(prev => !prev);
+    const handleModal = () => setIsShowModal((prev) => !prev);
 
     return (
         <>
             <section className="launch-date">
                 <div className="launch-date__wrapper">
                     <picture>
-                        <source media="(max-width: 800px)" srcSet={ballMobile} type="image/webp" />
-                        <source media="(min-width: 800px)" srcSet={ball} type="image/webp" />
-                        <source media="(max-width: 800px)" srcSet={webkitBallMobile} />
+                        <source
+                            media="(max-width: 800px)"
+                            srcSet={ballMobile}
+                            type="image/webp"
+                        />
+                        <source
+                            media="(min-width: 800px)"
+                            srcSet={ball}
+                            type="image/webp"
+                        />
+                        <source
+                            media="(max-width: 800px)"
+                            srcSet={webkitBallMobile}
+                        />
                         <img
                             className="launch-date__ball"
                             src={webkitBall}
@@ -41,15 +51,11 @@ export const LaunchDate: React.FC = () => {
                         />
                     </picture>
                     <div className="launch-date__information">
-                        <p
-                            className="launch-date__information__subtitle"
-                        >
+                        <p className="launch-date__information__subtitle">
                             Launch Date
                         </p>
-                        <h1
-                            className="launch-date__information__title"
-                        >
-                            1 November 20:00
+                        <h1 className="launch-date__information__title">
+                            November, TBA
                         </h1>
                         <button
                             className="launch-date__information__remind"
