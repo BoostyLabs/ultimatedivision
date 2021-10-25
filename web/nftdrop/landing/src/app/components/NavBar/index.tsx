13462ec1dbbac2a203952e07596c4d9c7218e34d
TarasPotsikaylo <potsikaylot@gmail.com>
2021-10-25 18:14:42 +0300

    some fix from UI designer

--- web/nftdrop/landing/src/app/components/NavBar/index.tsx
+++ web/nftdrop/landing/src/app/components/NavBar/index.tsx
@@ -1,19 +1,24 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { MintButton } from '@components/common/MintButton';
+import { MintButton } from "@components/common/MintButton";
 
-import NavBarLogo from '@static/images/navbar/navbar-logo.png';
-import { Cross, DropdownNavBar } from '@static/images/navbar/svg';
+import NavBarLogo from "@static/images/navbar/navbar-logo.png";
+import { Cross, DropdownNavBar } from "@static/images/navbar/svg";
 
-import './index.scss';
+import "./index.scss";
 
 export const Navbar: React.FC = () => {
     const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
 
-    const navBarItems: Array<string> = ['Home', 'Metaverse', 'About', 'Cards', 'Roadmap'];
+    const navBarItems: Array<string> = [
+        "Metaverse",
+        "About",
+        "Cards",
+        "Roadmap",
+    ];
 
     return (
         <div className="ultimatedivision-navbar">
@@ -30,21 +35,28 @@ export const Navbar: React.FC = () => {
                     {dropdownMenu ? <Cross /> : <DropdownNavBar />}
                 </div>
                 <ul
-                    className={`ultimatedivision-navbar__items${dropdownMenu ? '-active' : ''}`}
+                    className={`ultimatedivision-navbar__items${
+                        dropdownMenu ? "-active" : ""
+                    }`}
                 >
-                    {navBarItems.map((item, index) =>
+                    {navBarItems.map((item, index) => (
                         <li
                             key={index}
-                            className={`ultimatedivision-navbar__items${dropdownMenu ? '-active' : ''}__item`}
+                            className={`ultimatedivision-navbar__items${
+                                dropdownMenu ? "-active" : ""
+                            }__item`}
                         >
                             <a
                                 href={`#${item.toLowerCase()}`}
-                                className={`ultimatedivision-navbar__items${dropdownMenu ? '-active' : ''}__item__icon`}
+                                className={`ultimatedivision-navbar__items${
+                                    dropdownMenu ? "-active" : ""
+                                }__item__icon`}
                                 onClick={() => setDropdownMenu(false)}
                             >
                                 {item}
                             </a>
-                        </li>)}
+                        </li>
+                    ))}
                 </ul>
                 <MintButton />
             </div>
