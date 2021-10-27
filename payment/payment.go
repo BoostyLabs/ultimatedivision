a8432b56c9f19eb565087187f18523cf4e714bd2
Bogdan Serdinov <bogdan.serdinov.2002@gmail.com>
2021-10-28 01:40:18 +0300

    added interface payment

---
+++ payment/payment.go
@@ -0,0 +1,29 @@
+// Copyright (C) 2021 Creditor Corp. Group.
+// See LICENSE for copying information.
+
+package payment
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"ultimatedivision/lootboxes"
+	"ultimatedivision/pkg/cryptoutils"
+)
+
+// Payment provides access to all operations related to currency.
+type Payment interface {
+	// TopUpBalance tops up balance of user.
+	TopUpBalance(ctx context.Context, userID uuid.UUID, walletAddress cryptoutils.Address) error
+	// Withdraw withdraws money to a crypto wallet.
+	Withdraw(ctx context.Context, uuid2 uuid.UUID, walletAddress cryptoutils.Address) error
+	// BuyLootbox buys certain lootbox.
+	BuyLootbox(ctx context.Context, userID uuid.UUID, lootboxType lootboxes.Type, walletAddress cryptoutils.Address) error
+	// BuyCard buys certain card.
+	BuyCard(ctx context.Context, uuid2, lotID uuid.UUID, walletAddress cryptoutils.Address) error
+	// SellCard sells cards.
+	SellCard(ctx context.Context, userID uuid.UUID, cardID uuid.UUID, walletAddress cryptoutils.Address) error
+	// MakeBid makes bid on lot in marketplace.
+	MakeBid(ctx context.Context, userID, lotID uuid.UUID, walletAddress cryptoutils.Address) error
+}
