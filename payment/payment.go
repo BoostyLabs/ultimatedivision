580c2e193591d405a13074386d518bb74b1ed1b0
Bogdan Serdinov <bogdan.serdinov.2002@gmail.com>
2021-10-28 02:02:00 +0300

    interface payment divided in two

--- payment/payment.go
+++ payment/payment.go
@@ -11,19 +11,28 @@ import (
 	"ultimatedivision/lootboxes"
 	"ultimatedivision/pkg/cryptoutils"
 )
-
-// Payment provides access to all operations related to currency.
-type Payment interface {
+// Crypto provides access to all operations related to crypto payment.
+type Crypto interface {
 	// TopUpBalance tops up balance of user.
 	TopUpBalance(ctx context.Context, userID uuid.UUID, walletAddress cryptoutils.Address) error
 	// Withdraw withdraws money to a crypto wallet.
-	Withdraw(ctx context.Context, uuid2 uuid.UUID, walletAddress cryptoutils.Address) error
+	Withdraw(ctx context.Context, userID uuid.UUID, walletAddress cryptoutils.Address) error
+}
+
+// UDTToken provides access to all operations related to UDT token payment.
+type UDTToken interface {
 	// BuyLootbox buys certain lootbox.
-	BuyLootbox(ctx context.Context, userID uuid.UUID, lootboxType lootboxes.Type, walletAddress cryptoutils.Address) error
+	BuyLootbox(ctx context.Context, userID uuid.UUID, lootboxType lootboxes.Type) error
 	// BuyCard buys certain card.
-	BuyCard(ctx context.Context, uuid2, lotID uuid.UUID, walletAddress cryptoutils.Address) error
+	BuyCard(ctx context.Context, uuid2, lotID uuid.UUID) error
 	// SellCard sells card.
-	SellCard(ctx context.Context, userID uuid.UUID, cardID uuid.UUID, walletAddress cryptoutils.Address) error
+	SellCard(ctx context.Context, userID uuid.UUID, cardID uuid.UUID) error
 	// MakeBid makes bid on lot in the marketplace.
-	MakeBid(ctx context.Context, userID, lotID uuid.UUID, walletAddress cryptoutils.Address) error
+	MakeBid(ctx context.Context, userID, lotID uuid.UUID) error
+}
+
+// Payment provides access to all operations related to currency.
+type Payment interface {
+	Crypto
+	UDTToken
 }
