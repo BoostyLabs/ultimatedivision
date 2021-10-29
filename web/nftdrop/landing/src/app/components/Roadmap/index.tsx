6e2c4771bb01c19342a158a0a686e2e8ab51a45e
TarasPotsikaylo <potsikaylot@gmail.com>
2021-10-29 11:06:31 +0300

    animation flag part#1

--- web/nftdrop/landing/src/app/components/Roadmap/index.tsx
+++ web/nftdrop/landing/src/app/components/Roadmap/index.tsx
@@ -1,46 +1,47 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import React from 'react';
+import React from "react";
 
-import { RoadmapPoint } from './RoadmapPoint';
+import { RoadmapPoint } from "./RoadmapPoint";
+import { AnimationImage } from "@components/common/AnimationImage";
 
-import footer from '@static/images/roadmap/bottom.svg';
-import flag from '@static/images/roadmap/flag.svg';
+import animationFlagData from "@static/images/roadmap/flag-animation/data.json";
 
-import './index.scss';
+import footer from "@static/images/roadmap/bottom.svg";
+
+import "./index.scss";
 
 export const Roadmap: React.FC = () => {
-
     const dataList = [
         {
             id: 1,
-            date: '2021 Q4',
+            date: "2021 Q4",
             points: [
-                'Ultimate Division Metaverse MVP launch',
-                'Play to Earn mechanics available to players',
-                'UDT (Ultimate Division Token) introduced'
+                "Ultimate Division Metaverse MVP launch",
+                "Play to Earn mechanics available to players",
+                "UDT (Ultimate Division Token) introduced",
             ],
             done: true,
         },
         {
             id: 2,
-            date: '2022 Q1',
+            date: "2022 Q1",
             points: [
-                'Game in full swing',
-                'Management roles available',
-                'Club owners can tokenize their clubs and sell shares to other players',
-                'UD partnership with Top-5 Leagues',
+                "Game in full swing",
+                "Management roles available",
+                "Club owners can tokenize their clubs and sell shares to other players",
+                "UD partnership with Top-5 Leagues",
             ],
             done: false,
         },
         {
             id: 3,
-            date: '2022 Q2',
+            date: "2022 Q2",
             points: [
-                'Advanced gameplay introduced',
-                'Local competitions launched',
-                'DAO governance adopted',
+                "Advanced gameplay introduced",
+                "Local competitions launched",
+                "DAO governance adopted",
             ],
             done: false,
         },
@@ -49,19 +50,20 @@ export const Roadmap: React.FC = () => {
     return (
         <section className="roadmap">
             <div className="roadmap__wrapper">
-                <h2 className="roadmap__title">
-                    Development Roadmap
-                </h2>
+                <h2 className="roadmap__title">Development Roadmap</h2>
                 <div className="roadmap__road">
                     {dataList.map((item) => (
-                        <RoadmapPoint
-                            key={item.id} item={item} />
+                        <RoadmapPoint key={item.id} item={item} />
                     ))}
                 </div>
-                <img
-                    className="roadmap__flag"
-                    src={flag}
-                    alt=""
+                <AnimationImage
+                    className={"roadmap__flag"}
+                    heightFrom={1000}
+                    heightTo={-300}
+                    loop={false}
+                    animationData={animationFlagData}
+                    animationImages={[]}
+                    isNeedScrollListener={false}
                 />
             </div>
             <img
@@ -69,6 +71,6 @@ export const Roadmap: React.FC = () => {
                 src={footer}
                 alt="bottom texture"
             />
-        </section >
+        </section>
     );
 };
