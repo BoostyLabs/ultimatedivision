c4a008e82972767bbda0050e36d310ca00f2dfe6
Oleh Perehuda <peregudson97@gmail.com>
2021-11-01 10:46:08 +0200

    added const variables

--- web/console/src/app/components/common/Notification/index.tsx
+++ web/console/src/app/components/common/Notification/index.tsx
@@ -1,25 +1,25 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, ToastPosition } from 'react-toastify';
 
 import './index.scss';
 
 export const Notification: React.FC = () => {
     /** variables describes notification behaviour */
-    const POSITION: string = 'top-right';
+    const POSITION: ToastPosition = 'top-right';
     const AUTO_CLOSE_TIME: number = 5000;
     const IS_NEWEST_ON_TOP: boolean = false;
     const IS_CLOSED_ON_CLICK: boolean = false;
     const IS_RIGHT_TO_LEFT_LAYOUT: boolean = false;
 
     return <ToastContainer
-        position="top-right"
-        autoClose={5000}
+        position={POSITION}
+        autoClose={AUTO_CLOSE_TIME}
         hideProgressBar
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
+        newestOnTop={IS_NEWEST_ON_TOP}
+        closeOnClick={IS_CLOSED_ON_CLICK}
+        rtl={IS_RIGHT_TO_LEFT_LAYOUT}
         pauseOnFocusLoss
         pauseOnHover
     />
