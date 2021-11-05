e9d0b92ec4baf680794350dd53021f08abfc9d4f
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-11-05 16:03:19 +0200

    changed name variable

--- pkg/cryptoutils/cryptoutils_test.go
+++ pkg/cryptoutils/cryptoutils_test.go
@@ -28,7 +28,7 @@ func TestCryptoUtils(t *testing.T) {
 		"privateKey":      "5aefce0a2d473f59578fa7dee6a122d6509af1e0f79fcbee700dfcfeddabe4cc",
 		"signature":       "c2ab46d5981fe2fe951a63240af99a639ca13ea9afa1ee640d418690eae178215571ef9d318cc5b79d2c9e184e036f513aea7edcd6517cca75c4790d9ead45fc1b",
 	}
-	tokenId := 2
+	tokenID := 2
 
 	t.Run("GenerateSignature", func(t *testing.T) {
 		privateKeyECDSA, err := crypto.HexToECDSA(value1["privateKey"])
@@ -50,7 +50,7 @@ func TestCryptoUtils(t *testing.T) {
 		signature, err := cryptoutils.GenerateSignatureWithToken(
 			cryptoutils.Address(value2["addressWallet"]),
 			cryptoutils.Address(value2["addressContract"]),
-			tokenId,
+			tokenID,
 			privateKeyECDSA,
 		)
 		require.NoError(t, err)
