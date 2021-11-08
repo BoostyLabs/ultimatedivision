ed7b5bd357029532290df20d44369ef5ee23f6b8
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-11-08 11:08:57 +0200

    cards/nfts: remove nft struct to pkg lib

--- cards/nfts/nfts.go
+++ cards/nfts/nfts.go
@@ -10,6 +10,7 @@ import (
 	"github.com/zeebo/errs"
 
 	"ultimatedivision/pkg/cryptoutils"
+	"ultimatedivision/pkg/nft"
 )
 
 // ErrNoNFT indicates that nft token does not exist.
@@ -20,7 +21,7 @@ var ErrNoNFT = errs.Class("nft does not exist")
 // architecture: Storage
 type Storage interface {
 	// Save saves nft in the storage.
-	Save(ctx context.Context, nft NFT) error
+	Save(ctx context.Context, nft nft.NFT) error
 }
 
 // DB is exposing access to cards db.
@@ -47,22 +48,6 @@ type NFTWaitList struct {
 	CardID   uuid.UUID             `json:"cardId"`
 	Wallet   cryptoutils.Address   `json:"wallet"`
 	Password cryptoutils.Signature `json:"password"`
-}
-
-// NFT entity describes nft token format erc-721.
-type NFT struct {
-	Attributes  []Attribute `json:"attributes"`
-	Description string      `json:"description"`
-	ExternalURL string      `json:"external_url"`
-	Image       string      `json:"image"`
-	Name        string      `json:"name"`
-}
-
-// Attribute entity describes attributes for nft token.
-type Attribute struct {
-	TraitType string      `json:"trait_type"`
-	Value     interface{} `json:"value"`
-	MaxValue  interface{} `json:"max_value,omitempty"`
 }
 
 // MaxValueGameParameter indicates that max value game parameter is 100.
