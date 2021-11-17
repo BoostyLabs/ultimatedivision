7cd91b153ab9d8eb2a76c93f7a0edae7da2e0647
TarasPotsikaylo <potsikaylot@gmail.com>
2021-11-17 18:12:11 +0200

    added goals time

--- web/console/src/app/views/Match/PlayersScore/index.tsx
+++ web/console/src/app/views/Match/PlayersScore/index.tsx
@@ -1,24 +1,24 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import Player from '@/app/static/img/match/player.svg';
+import Player from "@/app/static/img/match/player.svg";
 
-import './index.scss';
+import "./index.scss";
 
 export const PlayersScore: React.FC = () => {
     // TODO: Mock data players score.
     const playersScoreTeam1 = [
-        { logo: Player, name: 'Ronalculus', goals: 1 },
-        { logo: Player, name: 'Ronalculus', goals: 3 },
-        { logo: Player, name: 'Ronalculus', goals: 2 },
-        { logo: Player, name: 'Ronalculus', goals: 2 },
+        { logo: Player, name: "Ronalculus", goalTime: "18:00", goals: 1 },
+        { logo: Player, name: "Ronalculus", goalTime: "25:01", goals: 3 },
+        { logo: Player, name: "Ronalculus", goalTime: "44:13", goals: 2 },
+        { logo: Player, name: "Ronalculus", goalTime: "99:99", goals: 2 },
     ];
 
     // TODO: Mock data players score.
     const playersScoreTeam2 = [
-        { logo: Player, name: 'Ronalculus', goals: 2 },
-        { logo: Player, name: 'Ronalculus', goals: 2 },
-        { logo: Player, name: 'Ronalculus', goals: 10 },
+        { logo: Player, name: "Ronalculus", goalTime: "01:00", goals: 2 },
+        { logo: Player, name: "Ronalculus", goalTime: "34:58", goals: 2 },
+        { logo: Player, name: "Ronalculus", goalTime: "82:44", goals: 10 },
     ];
 
     return (
@@ -26,38 +26,36 @@ export const PlayersScore: React.FC = () => {
             <div className="match__players-score__team-1">
                 <span className="match__players-score__title">Team 1</span>
                 {playersScoreTeam1 &&
-                    playersScoreTeam1.map((player, index) =>
+                    playersScoreTeam1.map((player, index) => (
                         <div className="player" key={index}>
                             <img
                                 src={player.logo}
                                 alt={`${player.name} player`}
                             ></img>
-                            <span className="player__name">
-                                {player.name}
+                            <span className="player__name">{player.name}</span>
+                            <span className="player__goal-time">
+                                {player.goalTime}
                             </span>
-                            <div className="player__goals">
-                                {player.goals}
-                            </div>
+                            <div className="player__goals">{player.goals}</div>
                         </div>
-                    )}
+                    ))}
             </div>
             <div className="match__players-score__team-2">
                 <span className="match__players-score__title">Team 2</span>
                 {playersScoreTeam2 &&
-                    playersScoreTeam2.map((player, index) =>
+                    playersScoreTeam2.map((player, index) => (
                         <div className="player" key={index}>
                             <img
                                 src={player.logo}
                                 alt={`${player.name} player`}
                             ></img>
-                            <span className="player__name">
-                                {player.name}
+                            <span className="player__name">{player.name}</span>
+                            <span className="player__goal-time">
+                                {player.goalTime}
                             </span>
-                            <div className="player__goals">
-                                {player.goals}
-                            </div>
+                            <div className="player__goals">{player.goals}</div>
                         </div>
-                    )}
+                    ))}
             </div>
         </div>
     );
