730369cdcb6e2cfbe9c8654e10c2da831dc48676
TarasPotsikaylo <potsikaylot@gmail.com>
2021-11-19 13:22:08 +0200

    Fixed remarks and styled scrollbar

--- web/console/src/app/views/Match/GoalScorers/index.tsx
+++ web/console/src/app/views/Match/GoalScorers/index.tsx
@@ -1,55 +1,48 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { GoalScorersTeam } from '@/app/views/Match/GoalScorers/GoalScorersTeam';
+import { GoalScorersTeam } from "@/app/views/Match/GoalScorers/GoalScorersTeam";
 
-import Player from '@/app/static/img/match/player.svg';
+import Player from "@/app/static/img/match/player.svg";
 
-import './index.scss';
+import "./index.scss";
 
 /** The maximum number of players scoring a goal, at which a block scroll is not needed. */
-const SCORED_PLAYERS_COUNT: number = 3;
+const MAX_SCORED_PLAYERS_COUNT: number = 3;
 
 export const GoalScorers: React.FC = () => {
-    const [isVisibleScroll, setIsVisibleScroll] = useState<{
+    const [visabilityTeamsAreaScroll, setVisabilityTeamsAreaScroll] = useState<{
         firstTeam: boolean;
         secondTeam: boolean;
     }>({ firstTeam: false, secondTeam: false });
 
     // TODO: Mock data players score.
-    const firstTeamGoalScorers = [
-        { logo: Player, name: 'Ronalculus', goalTime: '18:00', goals: 1 },
-        { logo: Player, name: 'Ronalculus', goalTime: '25:01', goals: 2 },
-        { logo: Player, name: 'Ronalculus', goalTime: '44:13', goals: 3 },
-        { logo: Player, name: 'Ronalculus', goalTime: '55:99', goals: 4 },
-        { logo: Player, name: 'Ronalculus', goalTime: '78:99', goals: 5 },
-        { logo: Player, name: 'Ronalculus', goalTime: '88:99', goals: 6 },
-        { logo: Player, name: 'Ronalculus', goalTime: '98:99', goals: 7 },
-        { logo: Player, name: 'Ronalculus', goalTime: '99:99', goals: 8 },
+    const firstGoalScorersTeam = [
+        { logo: Player, name: "Ronalculus", goalTime: "18:00", goals: 1 },
+        { logo: Player, name: "Ronalculus", goalTime: "25:01", goals: 2 },
+        { logo: Player, name: "Ronalculus", goalTime: "44:13", goals: 3 },
+        { logo: Player, name: "Ronalculus", goalTime: "55:99", goals: 4 },
+        { logo: Player, name: "Ronalculus", goalTime: "78:99", goals: 5 },
+        { logo: Player, name: "Ronalculus", goalTime: "88:99", goals: 6 },
+        { logo: Player, name: "Ronalculus", goalTime: "98:99", goals: 7 },
+        { logo: Player, name: "Ronalculus", goalTime: "99:99", goals: 8 },
     ];
 
     // TODO: Mock data players score.
-    const secondTeamGoalScorers = [
-        { logo: Player, name: 'Ronalculus', goalTime: '01:00', goals: 1 },
-        { logo: Player, name: 'Ronalculus', goalTime: '34:58', goals: 2 },
-        { logo: Player, name: 'Ronalculus', goalTime: '82:44', goals: 3 },
+    const secondGoalScorersTeam = [
+        { logo: Player, name: "Ronalculus", goalTime: "01:00", goals: 1 },
+        { logo: Player, name: "Ronalculus", goalTime: "34:58", goals: 2 },
+        { logo: Player, name: "Ronalculus", goalTime: "82:44", goals: 3 },
     ];
 
     useEffect(() => {
-        const copyIsVisibleScroll = { ...isVisibleScroll };
-
         /** If the length of the array of players who scored a goal is more than 3 - add a scroll for the block. */
-        if (firstTeamGoalScorers.length > SCORED_PLAYERS_COUNT) {
-            copyIsVisibleScroll.firstTeam = true;
-        }
-
-        if (secondTeamGoalScorers.length > SCORED_PLAYERS_COUNT) {
-            copyIsVisibleScroll.secondTeam = true;
-        }
-
-        setIsVisibleScroll(copyIsVisibleScroll);
+        setVisabilityTeamsAreaScroll({
+            firstTeam: firstGoalScorersTeam.length > MAX_SCORED_PLAYERS_COUNT,
+            secondTeam: secondGoalScorersTeam.length > MAX_SCORED_PLAYERS_COUNT,
+        });
     }, []);
 
     return (
@@ -58,20 +51,20 @@ export const GoalScorers: React.FC = () => {
                 <span className="match__goal-scorers__title">Team 1</span>
                 <div
                     className={`scores${
-                        isVisibleScroll.firstTeam ? '-scroll' : ''
+                        visabilityTeamsAreaScroll.firstTeam ? "-scroll" : ""
                     }`}
                 >
-                    <GoalScorersTeam team={firstTeamGoalScorers} />
+                    <GoalScorersTeam team={firstGoalScorersTeam} />
                 </div>
             </div>
             <div className="match__goal-scorers__team-2">
                 <span className="match__goal-scorers__title">Team 2</span>
                 <div
                     className={`scores${
-                        isVisibleScroll.secondTeam ? '-scroll' : ''
+                        visabilityTeamsAreaScroll.secondTeam ? "-scroll" : ""
                     }`}
                 >
-                    <GoalScorersTeam team={secondTeamGoalScorers} />
+                    <GoalScorersTeam team={secondGoalScorersTeam} />
                 </div>
             </div>
         </div>
