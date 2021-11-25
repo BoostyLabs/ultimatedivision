b6a0c1f95802d56f4a949bfb61298aee889327d2
Oleh Perehuda <peregudson97@gmail.com>
2021-11-25 17:24:16 +0200

    deleted merged code

--- web/console/src/api/queue.ts
+++ web/console/src/api/queue.ts
@@ -1,18 +1,13 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
+import { WebSocketAPICLient } from './webSocketClient';
+
 /**
  * QueueClient is a ws implementation of users API.
  * Exposes queue-related functionality.
  */
-export class QueueClient {
-    public ws: WebSocket;
-    /** The WebSocket provides the API for creating and managing
-     * a websocket connection to a server and for sending and
-     * receiving data on the connection. */
-    public constructor(path: string) {
-        this.ws = new WebSocket(`ws://${path}/api/v0/queue`);
-    }
+export class QueueClient extends WebSocketAPICLient {
     /** sends action to confirm and reject match, finish search */
     public sendAction(action: string, squadId: string) {
         this.ws.send(JSON.stringify({ action, squadId }));
