3d08a252698ee79a1fdb359533b7f62abbd728a7
vladdezhniuk <vladdezhniuk@gmail.com>
2021-11-30 17:57:36 +0200

    duplicated selector deleted

--- web/console/src/app/components/common/PlayerCard/index.tsx
+++ web/console/src/app/components/common/PlayerCard/index.tsx
@@ -1,8 +1,8 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-export const PlayerCard: React.FC<{ className: string, id: string }> = ({className, id }) =>
+export const PlayerCard: React.FC<{ className: string; id: string }> = ({ className, id }) =>
     <img
         className={className}
         src={`${window.location.origin}/avatars/${id}.png`}
-    />
+    />;
