83d49de16e10545cea8e165b977ee36d23764f90
TarasPotsikaylo <potsikaylot@gmail.com>
2021-12-02 17:01:19 +0200

    ran linter

--- web/console/src/App.tsx
+++ web/console/src/App.tsx
@@ -1,14 +1,14 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { Suspense } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import { AboutMenu } from "@components/common/AboutMenu";
-import { Notification } from "@components/common/Notification";
-import Preloader from "@components/common/Preloader";
+import { AboutMenu } from '@components/common/AboutMenu';
+import { Notification } from '@components/common/Notification';
+import Preloader from '@components/common/Preloader';
 
-import { Routes } from "@/app/routes";
+import { Routes } from '@/app/routes';
 
 /** initial App setup */
 export function App() {
