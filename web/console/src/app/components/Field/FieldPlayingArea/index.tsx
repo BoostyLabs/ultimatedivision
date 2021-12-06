23941871cfdf9dd50baad0659a7f461b4f323590
vladdezhniuk <vladdezhniuk@gmail.com>
2021-12-06 18:26:16 +0200

    lint

--- web/console/src/app/components/Field/FieldPlayingArea/index.tsx
+++ web/console/src/app/components/Field/FieldPlayingArea/index.tsx
@@ -152,7 +152,7 @@ export const FieldPlayingArea: React.FC = () => {
                                     }
                                     key={index}
                                     className={`playing-area__${formation}__${isDefined ? 'card' : 'empty-card'
-                                        }`}
+                                    }`}
                                     onClick={() => handleClick(index)}
                                     onDragStart={(e) => dragStart(e, index)}
                                     onMouseUp={(e) => onMouseUp(e, index)}
