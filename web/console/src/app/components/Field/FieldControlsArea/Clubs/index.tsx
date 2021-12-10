ff8fc70373eefc83a7d5a055c92f3db56763ba38
TarasPotsikaylo <potsikaylot@gmail.com>
2021-12-10 20:14:18 +0200

    added styles for active club

--- web/console/src/app/components/Field/FieldControlsArea/Clubs/index.tsx
+++ web/console/src/app/components/Field/FieldControlsArea/Clubs/index.tsx
@@ -1,35 +1,33 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { Manager, Owner } from '@static/img/FieldPage/clubs';
+import { Manager, Owner } from "@static/img/FieldPage/clubs";
 
-import './index.scss';
+import "./index.scss";
 
 const Clubs: React.FC = () => {
-    const [activeClub, setActiveClub] = useState<string>('CLUB 1');
+    const [activeClub, setActiveClub] = useState<string>("CLUB 1");
     const [activeComposition, setActiveComposition] =
-        useState<string>('Composition 1');
+        useState<string>("Composition 1");
     const [isActiveDropdown, setIsActiveDropdown] = useState<boolean>(false);
     const [clubId, setClubId] = useState<number | null>(null);
 
     // TODO: Mock data
     const clubs: Array<{ logo: any; name: string; whose: string }> = [
-        { logo: <Owner />, name: 'CLUB 1', whose: 'owner' },
-        { logo: <Owner />, name: 'CLUB 2', whose: 'owner' },
-        { logo: <Owner />, name: 'CLUB 3', whose: 'owner' },
-        { logo: <Manager />, name: 'CLUB 4', whose: 'manager' },
-        { logo: <Manager />, name: 'CLUB 4', whose: 'manager' },
-        { logo: <Manager />, name: 'CLUB 4', whose: 'manager' },
+        { logo: <Owner />, name: "CLUB 1", whose: "owner" },
+        { logo: <Owner />, name: "CLUB 2", whose: "owner" },
+        { logo: <Owner />, name: "CLUB 3", whose: "owner" },
+        { logo: <Manager />, name: "CLUB 4", whose: "manager" },
     ];
 
     // TODO: Mock data
     const compositions: string[] = [
-        'Composition 1',
-        'Composition 2',
-        'Composition 3',
-        'Composition 4',
+        "Composition 1",
+        "Composition 2",
+        "Composition 3",
+        "Composition 4",
     ];
 
     const handleChooseComposition = (composition: string) => {
@@ -43,9 +41,11 @@ const Clubs: React.FC = () => {
                 {activeClub}
             </span>
             <div className="clubs">
-                {clubs.map((club, index) =>
+                {clubs.map((club, index) => (
                     <div
-                        className="club"
+                        className={`club${
+                            club.name === activeClub ? "-active" : ""
+                        }`}
                         key={index}
                         onClick={() => setActiveClub(club.name)}
                         onMouseLeave={() => setClubId(null)}
@@ -53,20 +53,20 @@ const Clubs: React.FC = () => {
                     >
                         {club.logo}
                         <span className="club__name">{club.name}</span>
-                        {clubId === index &&
+                        {clubId === index && (
                             <div className="club__info">
-                                {club.whose === 'owner'
+                                {club.whose === "owner"
                                     ? `are you the ${club.whose}`
-                                    : 'you are the manager'}
+                                    : "you are the manager"}
                             </div>
-                        }
+                        )}
                     </div>
-                )}
+                ))}
             </div>
             <div className="composition">
                 <div
                     className={`composition__choosed-item ${
-                        isActiveDropdown ? 'active-dropdown' : ''
+                        isActiveDropdown ? "active-dropdown" : ""
                     }`}
                     onClick={() => setIsActiveDropdown(!isActiveDropdown)}
                 >
@@ -74,10 +74,10 @@ const Clubs: React.FC = () => {
                 </div>
                 <div
                     className={`composition__list${
-                        isActiveDropdown ? '-active' : ''
+                        isActiveDropdown ? "-active" : ""
                     }`}
                 >
-                    {compositions.map((composition, index) =>
+                    {compositions.map((composition, index) => (
                         <div
                             className="composition__list-item"
                             key={index}
@@ -85,7 +85,7 @@ const Clubs: React.FC = () => {
                         >
                             <span>{composition}</span>
                         </div>
-                    )}
+                    ))}
                 </div>
             </div>
         </div>
