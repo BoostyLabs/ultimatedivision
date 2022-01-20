dd04a490230029bcd09a1c3f55d48ae6bb59cba3
Tarkovskyi <tarkovskynik@gmail.com>
2022-01-20 14:54:42 +0200

    get season by division ID changed to get current season

--- database/seasons.go
+++ database/seasons.go
@@ -143,11 +143,10 @@ func (seasonsDB *seasonsDB) Delete(ctx context.Context, id int) error {
 func (seasonsDB *seasonsDB) GetSeasonByDivisionID(ctx context.Context, divisionID uuid.UUID) (seasons.Season, error) {
 	query := `SELECT id, division_id, started_at, ended_at 
 	          FROM seasons 
-	          WHERE division_id=$1 AND started_at IN (SELECT MAX(started_at)
-	                                                  FROM seasons)`
+	          WHERE division_id=$1 AND ended_at=$2`
 	var season seasons.Season
 
-	row := seasonsDB.conn.QueryRowContext(ctx, query, divisionID)
+	row := seasonsDB.conn.QueryRowContext(ctx, query, divisionID, time.Time{})
 
 	err := row.Scan(&season.ID, &season.DivisionID, &season.StartedAt, &season.EndedAt)
 	if err != nil {
