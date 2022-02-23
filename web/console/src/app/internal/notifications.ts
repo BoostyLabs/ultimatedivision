9c0c1fa5682373d6619315bf4c3c5a9cdd09e944
Anna Tsybel <anna.tsybel@nure.ua>
2022-02-23 13:39:50 +0200

    web/console: Fix naming of variables

--- web/console/src/app/internal/notifications.ts
+++ web/console/src/app/internal/notifications.ts
@@ -2,7 +2,7 @@
 // See LICENSE for copying information.
 
 import { toast, ToastOptions } from 'react-toastify';
-import { InternalError, TooManyRequestError } from '@/api';
+import { InternalError, TooManyRequestsError } from '@/api';
 
 /** Code which indicates that 'eth_requestAccounts' already processing */
 const RPC_ERROR_CODE = -32002;
@@ -16,22 +16,21 @@ const notificationConfig:ToastOptions<{}> = {
 
 export function metamaskNotifications(error: any) {
     switch (error.code) {
-        case RPC_ERROR_CODE:
-            toast.error('Please open metamask manually!', notificationConfig);
-            break;
-        case DENIED_TRANSACTION_CODE:
-            toast.error('You denied transaction', notificationConfig);
-            break;
-        default: 
-            if (error instanceof TooManyRequestError) {
-                toast.error(error.message, notificationConfig);
-            }
-            else if (error instanceof InternalError) {
-                toast.error(error.message, notificationConfig);
-            }
-            else if (error.error.code === ALREADY_MINTED_ERROR_CODE) {
-                toast.error('Token already minted', notificationConfig);
-            }
-        
+    case RPC_ERROR_CODE:
+        toast.error('Please open metamask manually!', notificationConfig);
+        break;
+    case DENIED_TRANSACTION_CODE:
+        toast.error('You denied transaction', notificationConfig);
+        break;
+    default:
+        if (error instanceof TooManyRequestsError) {
+            toast.error(error.message, notificationConfig);
+        }
+        else if (error instanceof InternalError) {
+            toast.error(error.message, notificationConfig);
+        }
+        else if (error.error.code === ALREADY_MINTED_ERROR_CODE) {
+            toast.error('Token already minted', notificationConfig);
+        }
     }
 }
