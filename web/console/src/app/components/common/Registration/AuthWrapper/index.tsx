a93c70861e9a1b1761cfd49b6f1f05289be68fc3
Anna Tsybel <anna.tsybel@nure.ua>
2022-05-18 11:18:02 +0300

    web/console: add request to backend

--- web/console/src/app/components/common/Registration/AuthWrapper/index.tsx
+++ web/console/src/app/components/common/Registration/AuthWrapper/index.tsx
@@ -4,19 +4,27 @@ import { useHistory } from 'react-router-dom';
 import { vaclient } from '@/app/velas/service';
 import { toast } from 'react-toastify';
 
-import { AuthRouteConfig } from '@/app/routes';
+import { AuthRouteConfig, RouteConfig } from '@/app/routes';
 import { InternalError } from '@/api';
 
 import ulimatedivisionLogo from '@static/img/registerPage/ultimate.svg';
 
 import './index.scss';
+import { UsersClient } from '@/api/users';
+import { UsersService } from '@/users/service';
 
 const AuthWrapper = () => {
     const history = useHistory();
+    const usersClient = new UsersClient();
+    const usersService = new UsersService(usersClient);
 
-    const sendAuthData = (authResult: any) => {
+    const sendAuthData = async(authResult: any) => {
         try {
-            // TODO: add function sending data to backend.
+            await usersService.velasRegister(authResult);
+            const nonce = await usersService.velasNonce(authResult.access_token_payload.sub);
+            await usersService.velasLogin(nonce, authResult);
+            history.push(RouteConfig.MarketPlace.path);
+            window.location.reload();
         } catch (error) {
             if (error instanceof InternalError) {
                 history.push(AuthRouteConfig.SignIn.path);
