f31abe1a1fee55e904d6db7e7312a9b1f05ecfc7
Anna Tsybel <anna.tsybel@nure.ua>
2022-06-13 02:57:40 +0300

    web/console: implement game

--- web/console/src/app/views/FootballGame/index.tsx
+++ web/console/src/app/views/FootballGame/index.tsx
@@ -1,22 +1,13 @@
-// Copyright (C) 2021 Creditor Corp. Group.
+// Copyright (C) 2022 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { useState } from "react";
-import Unity, { UnityContext } from "react-unity-webgl";
+import Unity, { UnityContent } from "react-unity-webgl";
 
 import "./index.scss";
 
 const FootballGame: React.FC = () => {
-    const unityContext = new UnityContext({
-        companyName: "DefaultCompany",
-        productName: "Football2019",
-        loaderUrl: "/static/dist/webGl/UnityLoader.js",
-        dataUrl: "/static/dist/webGl/footbal.data.unityweb",
-        frameworkUrl: "/static/dist/webGl/footbal.wasm.framework.unityweb",
-        codeUrl: "/static/dist/webGl/footbal.wasm",
-    });
-
-    return <Unity unityContext={unityContext} className="unity-container" />;
+    const unityContext = new UnityContent("/static/dist/webGl/footbal.json", "/static/dist/webGl/UnityLoader.js");
+    return <Unity unityContent={unityContext} className="unity-container" />;
 };
 
 export default FootballGame;
