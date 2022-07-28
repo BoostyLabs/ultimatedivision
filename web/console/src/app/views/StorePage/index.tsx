f876c2e81b9ef7079308310bf596455ff9635b1b
Anna Tsybel <anna.tsybel@nure.ua>
2022-07-28 05:25:08 +0300

    web/console: change header style in opening boxes

--- web/console/src/app/views/StorePage/index.tsx
+++ web/console/src/app/views/StorePage/index.tsx
@@ -1,21 +1,32 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
+import { useEffect, useState } from 'react';
+
 import { LootboxContent } from '@/app/components/Store/LootboxContent';
 import { LootboxSelection } from '@/app/components/Store/LootboxSelection';
-import { useState } from 'react';
+
 import './index.scss';
 
 const Store = () => {
     const [isOpening, handleOpening] = useState(false);
 
+    const header = document.querySelector('.ultimatedivision-navbar');
+    useEffect(
+        () =>
+            isOpening
+                ? header?.classList.add('ultimatedivision-navbar__store-opening')
+                : header?.classList.remove('ultimatedivision-navbar__store-opening'),
+        [isOpening]
+    );
+
     return (
         <section className="store">
             {!isOpening ?
-                <>
+                <div className="store__content">
                     <h1>Box</h1>
                     <LootboxSelection handleOpening={handleOpening} />
-                </>
+                </div>
                 :
                 <LootboxContent handleOpening={handleOpening} />
             }
