ff2a98d0bbc3e9879f40420d72d841a23d1cbced
Anna Tsybel <anna.tsybel@nure.ua>
2022-09-28 15:37:14 +0300

    web/console: fix comments

--- web/console/src/app/components/home/Roadmap/RoadmapPoint/index.tsx
+++ web/console/src/app/components/home/Roadmap/RoadmapPoint/index.tsx
@@ -1,22 +1,16 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
+import { RoadmapStep } from '..';
 import './index.scss';
 
-export const RoadmapPoint: React.FC<{
-    item: {
-        step: string;
-        points: string[];
-        id: number;
-        done: boolean;
-    };
-}> = ({ item }) =>
+export const RoadmapPoint: React.FC<{ item: RoadmapStep }> = ({ item }) =>
     <div className={`roadmap-point__${item.step}`}>
         <div className={'roadmap-point'}>
             <p className="roadmap-point__step">{item.step}</p>
             <ul className="roadmap-point__list">
-                {item.points.map((point, index) =>
-                    <li className="roadmap-point__item" key={index}>
+                {item.points.map((point, _) =>
+                    <li className="roadmap-point__item" key={item.id}>
                         <div className="roadmap-point__item__bullet"></div>
                         <span className="roadmap-point__item__description">
                             {point}
--- web/console/src/app/components/home/Roadmap/index.tsx
+++ web/console/src/app/components/home/Roadmap/index.tsx
@@ -7,48 +7,42 @@ import roadmap from '@static/img/gameLanding/roadmap/roadmap.png';
 
 import './index.scss';
 
+/** Domain entity RoadmapStep implementation */
+export class RoadmapStep {
+    /** default RoadmapStep implementation */
+    constructor(
+        public id: string = '',
+        public step: string = '',
+        public points: string[] = []) { }
+}
+
 export const Roadmap: React.FC = () => {
-    const dataList = [
-        {
-            id: 1,
-            step: 'start',
-            points: [
-                'Ultimate Division Metaverse MVP launch',
-                'Play to Earn mechanics available to players',
-                'UDT (Ultimate Division Token) introduced',
-            ],
-            done: true,
-        },
-        {
-            id: 2,
-            step: 'middle',
-            points: [
-                'Game in full swing',
-                'Management roles available',
-                'Club owners can tokenize their clubs and sell shares to other players',
-                'UDT partnership with Top-5 Leagues',
-            ],
-            done: false,
-        },
-        {
-            id: 3,
-            step: 'finish',
-            points: [
-                'Advanced gameplay introduced',
-                'Local competitions launched',
-                'DAO governance adopted',
-            ],
-            done: false,
-        },
+    const roadmapSteps = [
+        new RoadmapStep('1', 'start', [
+            'Ultimate Division Metaverse MVP launch',
+            'Play to Earn mechanics available to players',
+            'UDT (Ultimate Division Token) introduced',
+        ]),
+        new RoadmapStep('2', 'middle', [
+            'Game in full swing',
+            'Management roles available',
+            'Club owners can tokenize their clubs and sell shares to other players',
+            'UDT partnership with Top-5 Leagues',
+        ]),
+        new RoadmapStep('3', 'finish', [
+            'Advanced gameplay introduced',
+            'Local competitions launched',
+            'DAO governance adopted',
+        ]),
     ];
 
     return (
         <section className="roadmap">
             <h2 className="roadmap__title">Our <span className="roadmap__title__second-part">Roadmap</span></h2>
-            {dataList.map((item) =>
+            {roadmapSteps.map((item: RoadmapStep) =>
                 <RoadmapPoint key={item.id} item={item} />
             )}
-            <img className="roadmap__image" src={roadmap} alt="roadmap"/>
+            <img className="roadmap__image" src={roadmap} alt="roadmap" />
         </section>
     );
 };
