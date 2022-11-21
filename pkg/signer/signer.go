a5a6120da3ceab284002dcedb035b60538c104d0
APrisiazhniuk <88141219+APrisiazhniuk@users.noreply.github.com>
2022-11-21 21:13:53 +0200

    remove GenerateNFTStoreSignature

--- pkg/signer/signer.go
+++ pkg/signer/signer.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/BoostyLabs/evmsignature"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/google/uuid"
 	"github.com/zeebo/errs"
 )
@@ -121,40 +120,4 @@ func (address Address) IsValidAddress() error {
 		return ErrCreateSignature.New("")
 	}
 	return nil
-}
-
-// GenerateNFTStoreSignature generates signature for user's wallet to buy nft in store.
-func GenerateNFTStoreSignature(nftStoreSignature NFTStoreSignature) (evmsignature.Signature, error) {
-	var values [][]byte
-
-	methodSelector := crypto.Keccak256Hash([]byte(nftStoreSignature.MethodName)).Bytes()[:4]
-
-	tokenIDStringWithZeros := evmsignature.CreateHexStringFixedLength(fmt.Sprintf("%x", nftStoreSignature.TokenID))
-	tokenIDByte, err := hex.DecodeString(string(tokenIDStringWithZeros))
-	if err != nil {
-		return "", ErrCreateSignature.Wrap(err)
-	}
-
-	valueStringWithZeros := evmsignature.CreateHexStringFixedLength(fmt.Sprintf("%x", nftStoreSignature.Value))
-	valueByte, err := hex.DecodeString(string(valueStringWithZeros))
-	if err != nil {
-		return "", ErrCreateSignature.Wrap(err)
-	}
-
-	values = append(values, methodSelector, nftStoreSignature.WalletAddress.Hash().Bytes(), nftStoreSignature.ContractAddress.Hash().Bytes(),
-		tokenIDByte, valueByte)
-
-	createSignature := evmsignature.CreateSignature{
-		Values:     values,
-		PrivateKey: nftStoreSignature.PrivateKey,
-	}
-
-	signatureByte, err := evmsignature.MakeSignature(createSignature)
-	if err != nil {
-		return "", ErrCreateSignature.Wrap(err)
-	}
-
-	signature, err := evmsignature.ReformSignature(signatureByte)
-
-	return signature, ErrCreateSignature.Wrap(err)
 }
