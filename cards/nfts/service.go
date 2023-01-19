bc6b113d6d762e3b9908a38e1fe5ab50332698be
APrisiazhniuk <88141219+APrisiazhniuk@users.noreply.github.com>
2023-01-19 23:23:22 +0200

    cards/nft: add parsing methods

--- cards/nfts/service.go
+++ cards/nfts/service.go
@@ -4,14 +4,23 @@
 package nfts
 
 import (
+	"bufio"
 	"context"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/BoostyLabs/evmsignature"
 	"github.com/google/uuid"
 	"github.com/zeebo/errs"
 
 	"ultimatedivision/cards"
+	contract "ultimatedivision/pkg/contractcasper"
+	"ultimatedivision/pkg/eventparsing"
 	"ultimatedivision/pkg/nft"
 )
 
@@ -23,14 +32,30 @@ var ErrNFTs = errs.Class("NFTs service error")
 // architecture: Service.
 type Service struct {
 	config Config
+	casper contract.Casper
 	nfts   DB
+	events *http.Client
 }
+
+const (
+	// WriteCLValueKey defines that transform key is WriteCLValue. This key stores the type and data of the transforming event.
+	WriteCLValueKey string = "WriteCLValue"
+	// BytesKey defines that WriteCLValue key is bytes. This key stores data of the transforming event.
+	BytesKey string = "bytes"
+)
 
 // NewService is a constructor for NFTs service.
 func NewService(config Config, nfts DB) *Service {
+	eventsClient := &http.Client{
+		Transport: &http.Transport{
+			DisableCompression: true,
+		},
+	}
+
 	return &Service{
 		config: config,
 		nfts:   nfts,
+		events: eventsClient,
 	}
 }
 
@@ -131,4 +156,176 @@ func (service *Service) Update(ctx context.Context, nft NFT) error {
 // Delete deletes nft token in the database.
 func (service *Service) Delete(ctx context.Context, cardID uuid.UUID) error {
 	return ErrNFTs.Wrap(service.nfts.Delete(ctx, cardID))
+}
+
+// SubscribeEvents is real time events streaming from blockchain to events subscribers.
+func (service *Service) SubscribeEvents(ctx context.Context) (EventVariant, error) {
+	var body io.Reader
+	req, err := http.NewRequest(http.MethodGet, service.config.EventNodeAddress, body)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	resp, err := service.events.Do(req)
+	if err != nil {
+		defer func() {
+			err = errs.Combine(err, resp.Body.Close())
+		}()
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	reader := bufio.NewReader(resp.Body)
+	rawBody, err := reader.ReadBytes('\n')
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	rawBody = []byte(strings.Replace(string(rawBody), "data:", "", 1))
+
+	var event contract.Event
+	_ = json.Unmarshal(rawBody, &event)
+
+	transforms := event.DeployProcessed.ExecutionResult.Success.Effect.Transforms
+	if len(transforms) == 0 {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+	for _, transform := range transforms {
+		eventFunds, err := service.parseEventFromTransform(event, transform)
+		if err != nil {
+			return eventFunds, ErrNFTs.Wrap(err)
+		}
+	}
+	return EventVariant{}, ErrNFTs.Wrap(err)
+}
+func (service *Service) parseEventFromTransform(event contract.Event, transform contract.Transform) (EventVariant, error) {
+	transformMap, ok := transform.Transform.(map[string]interface{})
+	if !ok {
+		return EventVariant{}, ErrNFTs.New("couldn't parse map to transform")
+	}
+
+	writeCLValue, ok := transformMap[WriteCLValueKey].(map[string]interface{})
+	if !ok {
+		return EventVariant{}, ErrNFTs.New("couldn't parse map to transform map")
+	}
+
+	bytes, ok := writeCLValue[BytesKey].(string)
+	if !ok {
+		return EventVariant{}, ErrNFTs.New("couldn't parse string to bytes key")
+	}
+
+	eventData := eventparsing.EventData{
+		Bytes: bytes,
+	}
+
+	eventType, err := eventData.GetEventType()
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	tokenContractAddress, err := hex.DecodeString(eventData.GetTokenContractAddress())
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	chainName, err := eventData.GetChainName()
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	chainAddress, err := eventData.GetChainAddress()
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	amount, err := eventData.GetAmount()
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+	amountStr := strconv.Itoa(amount)
+
+	userWalletAddress, err := hex.DecodeString(eventData.GetUserWalletAddress())
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	hash, err := hex.DecodeString(event.DeployProcessed.DeployHash)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	sender, err := hex.DecodeString(event.DeployProcessed.Account)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	blockNumber, err := service.casper.GetBlockNumberByHash(event.DeployProcessed.BlockHash)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	transactionInfo := TransactionInfo{
+		Hash:        hash,
+		BlockNumber: uint64(blockNumber),
+		Sender:      sender,
+	}
+
+	var eventFunds EventVariant
+	switch eventType {
+	case EventTypeIn.Int():
+		eventFunds = EventVariant{
+			Type: EventType(eventType),
+			EventFundsIn: EventFundsIn{
+				From: userWalletAddress,
+				To: Address{
+					NetworkName: chainName,
+					Address:     chainAddress,
+				},
+				Amount: amountStr,
+				Token:  tokenContractAddress,
+				Tx:     transactionInfo,
+			},
+		}
+	case EventTypeOut.Int():
+		eventFunds = EventVariant{
+			Type: EventType(eventType),
+			EventFundsOut: EventFundsOut{
+				From: Address{
+					NetworkName: chainName,
+					Address:     chainAddress,
+				},
+				To:     userWalletAddress,
+				Amount: amountStr,
+				Token:  tokenContractAddress,
+				Tx:     transactionInfo,
+			},
+		}
+	default:
+		return EventVariant{}, ErrNFTs.New("invalid event type")
+	}
+
+	tokenIn := hex.EncodeToString(eventFunds.EventFundsIn.Token)
+	eventFunds.EventFundsIn.Token, err = hex.DecodeString(eventparsing.TagHash.String() + tokenIn)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	from := hex.EncodeToString(eventFunds.EventFundsIn.From)
+	eventFunds.EventFundsIn.From, err = hex.DecodeString(eventparsing.TagAccount.String() + from)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	tokenOut := hex.EncodeToString(eventFunds.EventFundsOut.Token)
+	eventFunds.EventFundsOut.Token, err = hex.DecodeString(eventparsing.TagHash.String() + tokenOut)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	to := hex.EncodeToString(eventFunds.EventFundsOut.To)
+	eventFunds.EventFundsOut.To, err = hex.DecodeString(eventparsing.TagAccount.String() + to)
+	if err != nil {
+		return EventVariant{}, ErrNFTs.Wrap(err)
+	}
+
+	return eventFunds, nil
 }
