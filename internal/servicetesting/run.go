8aa0c35e169c5950db657c2ac412c2250ca1c04c
APrisiazhniuk <88141219+APrisiazhniuk@users.noreply.github.com>
2023-01-23 22:48:00 +0200

    remove user service testing/it is already done in user test

--- internal/servicetesting/run.go
+++ internal/servicetesting/run.go
@@ -10,24 +10,15 @@ import (
 	"ultimatedivision"
 	"ultimatedivision/database/dbtesting"
 	"ultimatedivision/internal/logger/zaplog"
-	"ultimatedivision/users"
 )
 
 // Run method will establish connection with db, create services, allows reconfiguring config and run tests.
 func Run(t *testing.T, setupConfig func() ultimatedivision.Config, test func(ctx context.Context, t *testing.T, ultimatedivision *ultimatedivision.Peer)) {
 	dbtesting.Run(t, func(ctx context.Context, t *testing.T, db ultimatedivision.DB) {
-		ultimatedivision := &ultimatedivision.Peer{
+		_ = &ultimatedivision.Peer{
 			Log:      zaplog.NewLog(),
 			Database: db,
 		}
 
-		var err error
-
-		ultimatedivision.Users.Service = users.NewService(db.Users())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		test(ctx, t, ultimatedivision)
 	})
 }
