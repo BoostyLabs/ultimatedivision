668508d253b9662c7b732c9aa008bbce5327ba8a
Anna Tsybel <anna.tsybel@nure.ua>
2023-03-10 15:15:10 +0200

    web/console: fix issues

--- web/console/src/app/static/img/FilterField/arrows.tsx
+++ web/console/src/app/static/img/FilterField/arrows.tsx
@@ -2,7 +2,7 @@
 export function ArrowIcon() {
     return (
         <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.5 1.75L6 6.25L10.5 1.75" stroke="#26373F" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M1.5 1.75L6 6.25L10.5 1.75" stroke="#26373F" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     );
 };
@@ -11,7 +11,7 @@ export function ArrowIcon() {
 export function ArrowActiveIcon() {
     return (
         <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.5 1.75L6 6.25L10.5 1.75" stroke="#F5FF63" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M1.5 1.75L6 6.25L10.5 1.75" stroke="#F5FF63" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     );
 };
--- web/console/src/app/static/img/UltimateDivisionPaginator/arrows.tsx
+++ web/console/src/app/static/img/UltimateDivisionPaginator/arrows.tsx
@@ -2,8 +2,8 @@
 export function ArrowNextIcon() {
     return (
         <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M6.66675 16H25.3334" stroke="#CBFB00" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M16 6.66602L25.3333 15.9993L16 25.3327" stroke="#CBFB00" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M6.66675 16H25.3334" stroke="#CBFB00" strokeWidth="2.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M16 6.66602L25.3333 15.9993L16 25.3327" stroke="#CBFB00" strokeWidth="2.66667" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     );
 };
@@ -12,8 +12,8 @@ export function ArrowNextIcon() {
 export function ArrowPrevIcon() {
     return (
         <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M25.3333 16H6.66665" stroke="#CBFB00" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M16 6.66602L6.66667 15.9993L16 25.3327" stroke="#CBFB00" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M25.3333 16H6.66665" stroke="#CBFB00" strokeWidth="2.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M16 6.66602L6.66667 15.9993L16 25.3327" stroke="#CBFB00" strokeWidth="2.66667" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     );
 };
