fb4f43a814aa709aa5397525a347cd8eb49681f4
APrisiazhniuk <88141219+APrisiazhniuk@users.noreply.github.com>
2023-03-30 15:46:29 +0300

    comment if nft exist

--- console/consoleserver/controllers/marketplace.go
+++ console/consoleserver/controllers/marketplace.go
@@ -181,10 +181,11 @@ func (controller *Marketplace) CreateLot(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// TODO: remove after adding Casper contract.
 	// if _, err = controller.marketplace.GetNFTByCardID(ctx, createLot.CardID); err != nil {
 	//	controller.log.Error("there is no such NFT", ErrMarketplace.Wrap(err))
 	//	controller.serveError(w, http.StatusBadRequest, ErrMarketplace.Wrap(err))
-	// }.
+	// } .
 
 	createLot.UserID = claims.UserID
 
