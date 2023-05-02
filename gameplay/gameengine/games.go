24115fe6941fa6f3236e4276fbbf0b8578844140
Tarkovskyi <tarkovskynik@gmail.com>
2023-05-03 02:30:08 +0300

    gameplay/gameengine-added move action

--- gameplay/gameengine/games.go
+++ gameplay/gameengine/games.go
@@ -37,7 +37,7 @@ const (
 // Game defines game data.
 type Game struct {
 	MatchID  uuid.UUID
-	GameInfo []CardIDWithPosition
+	GameInfo CardIDsWithPositionWithBallPosition
 }
 
 // CardIDWithPosition defines card ID with possible moves cells for game.
