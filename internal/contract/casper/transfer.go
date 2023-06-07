532160cb62acd8762056927d80c9fadb00090875
Dmytro <dmytroyakimuk@gmail.com>
2023-06-07 13:44:38 +0300

    fixed internal casper lib

--- internal/contract/casper/transfer.go
+++ internal/contract/casper/transfer.go
@@ -6,7 +6,6 @@ package casper
 import (
 	"context"
 	"encoding/hex"
-	"fmt"
 	"math/big"
 	"strings"
 
@@ -18,25 +17,6 @@ import (
 
 	contract "ultimatedivision/pkg/contractcasper"
 )
-
-// SetSignerRequest describes values to calls setSigner method.
-type SetSignerRequest struct {
-	PublicKey                   keypair.PublicKey
-	ChainName                   string
-	StandardPaymentForBridgeOut int64
-	BridgeContractPackageHash   string
-	Value                       string
-}
-
-// SetFinalListingRequest describes values to calls final listing method.
-type SetFinalListingRequest struct {
-	PublicKey                   keypair.PublicKey
-	ChainName                   string
-	NftContractHash             string
-	TokenID                     string
-	StandardPaymentForBridgeOut int64
-	BridgeContractPackageHash   string
-}
 
 // Transfer describes sign func to sign transaction and casper client to send transaction.
 type Transfer struct {
@@ -52,166 +32,6 @@ func NewTransfer(casper contract.Casper, sign func([]byte) ([]byte, error)) *Tra
 		sign:   sign,
 	}
 }
-
-// SetFinalListing send NFT to winner.
-func (t *Transfer) SetFinalListing(ctx context.Context, req SetFinalListingRequest) (string, error) {
-	deployParams := sdk.NewDeployParams(req.PublicKey, strings.ToLower(req.ChainName), nil, 0)
-	// ціна на газ ліміт.
-	payment := sdk.StandardPayment(big.NewInt(req.StandardPaymentForBridgeOut))
-	fmt.Println("payment ------->", payment)
-	// NFT contract hash, where we mint NFT.
-	nftContract := types.CLValue{
-		Type:   types.CLTypeString,
-		String: &req.NftContractHash,
-	}
-	nftContractBytes, err := serialization.Marshal(nftContract)
-	if err != nil {
-		return "", err
-	}
-
-	tokenIDFromStringByteSlice := []byte("$\u0000\u0000\u0000" + req.TokenID)
-	fmt.Println("tokenIDFromStringByteSlice ------->", tokenIDFromStringByteSlice)
-	args := map[string]sdk.Value{
-		"nft_contract_hash": {
-			Tag:         types.CLTypeString,
-			IsOptional:  false,
-			StringBytes: hex.EncodeToString(nftContractBytes),
-		},
-		"token_id": {
-			Tag:         types.CLTypeString,
-			IsOptional:  false,
-			StringBytes: hex.EncodeToString(tokenIDFromStringByteSlice),
-		},
-	}
-	fmt.Println("args ------->", args)
-	keyOrder := []string{"nft_contract_hash", "token_id"}
-	runtimeArgs := sdk.NewRunTimeArgs(args, keyOrder)
-
-	// contract that we are using.
-	contractHexBytes, err := hex.DecodeString(req.BridgeContractPackageHash)
-	if err != nil {
-		return "", err
-	}
-
-	var contractHashBytes [32]byte
-	copy(contractHashBytes[:], contractHexBytes)
-	session := sdk.NewStoredContractByHash(contractHashBytes, "final_listing", *runtimeArgs)
-	fmt.Println("session ------->", payment)
-	deploy := sdk.MakeDeploy(deployParams, payment, session)
-	fmt.Println("deploy ------->", payment)
-	signedTx, err := t.sign(deploy.Hash)
-	if err != nil {
-		return "", err
-	}
-
-	signatureKeypair := keypair.Signature{
-		Tag:           keypair.KeyTagEd25519,
-		SignatureData: signedTx,
-	}
-	approval := sdk.Approval{
-		Signer:    req.PublicKey,
-		Signature: signatureKeypair,
-	}
-	deploy.Approvals = append(deploy.Approvals, approval)
-	hash, err := t.casper.PutDeploy(*deploy)
-	if err != nil {
-		return "", err
-	}
-	fmt.Println("hash ------->", hash)
-
-	return hash, nil
-}
-
-// SetAcceptOffer send NFT to winner.
-func (t *Transfer) SetAcceptOffer(ctx context.Context, req SetSignerRequest) (string, error) {
-	deployParams := sdk.NewDeployParams(req.PublicKey, strings.ToLower(req.ChainName), nil, 0)
-	payment := sdk.StandardPayment(big.NewInt(req.StandardPaymentForBridgeOut))
-
-	value := types.CLValue{
-		Type:   types.CLTypeString,
-		String: &req.Value,
-	}
-	valueBytes, err := serialization.Marshal(value)
-	if err != nil {
-		return "", err
-	}
-
-	args := map[string]sdk.Value{
-		"signer": {
-			Tag:         types.CLTypeString,
-			IsOptional:  false,
-			StringBytes: hex.EncodeToString(valueBytes),
-		},
-	}
-
-	//amountBytes, err := serialization.Marshal(nftContractHash)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//args2 := map[string]sdk.Value{
-	//	"nft_contract_hash": {
-	//		Tag:         types.CLTypeString,
-	//		IsOptional:  false,
-	//		StringBytes: hex.EncodeToString(amountBytes),
-	//	},
-	//	"token_id": {
-	//		Tag:         types.CLTypeString,
-	//		IsOptional:  false,
-	//		StringBytes: hex.EncodeToString(gasCommissionBytes),
-	//	},
-	//	"erc20_contract": {
-	//		Tag:         types.CLTypeU256,
-	//		IsOptional:  false,
-	//		StringBytes: hex.EncodeToString(deadlineBytes),
-	//	},
-	//}
-
-	keyOrder := []string{"nft_contract_hash", "token_id", "erc20_contract"}
-	runtimeArgs := sdk.NewRunTimeArgs(args, keyOrder)
-
-	contractHexBytes, err := hex.DecodeString(req.BridgeContractPackageHash)
-	if err != nil {
-		return "", err
-	}
-
-	var contractHashBytes [32]byte
-	copy(contractHashBytes[:], contractHexBytes)
-	session := sdk.NewStoredContractByHash(contractHashBytes, "accept_offer", *runtimeArgs)
-
-	deploy := sdk.MakeDeploy(deployParams, payment, session)
-
-	signedTx, err := t.sign(deploy.Hash)
-	if err != nil {
-		return "", err
-	}
-
-	signatureKeypair := keypair.Signature{
-		Tag:           keypair.KeyTagEd25519,
-		SignatureData: signedTx,
-	}
-	approval := sdk.Approval{
-		Signer:    req.PublicKey,
-		Signature: signatureKeypair,
-	}
-	deploy.Approvals = append(deploy.Approvals, approval)
-
-	hash, err := t.casper.PutDeploy(*deploy)
-	if err != nil {
-		return "", err
-	}
-
-	return hash, nil
-}
-
-////
-////
-////
-////
-////
-////
-////
-////
 
 type MintOneRequest struct {
 	PublicKey              keypair.PublicKey
--- internal/contract/casper/transfer_test.go
+++ internal/contract/casper/transfer_test.go
@@ -8,7 +8,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/big"
-	"os"
 	"testing"
 
 	casper_ed25519 "github.com/casper-ecosystem/casper-golang-sdk/keypair/ed25519"
@@ -19,166 +18,14 @@ import (
 	contract "ultimatedivision/pkg/contractcasper"
 )
 
-func TestCasper_SetAcceptOffer(t *testing.T) {
-
-	var (
-		casperNodeAddress = "http://136.243.187.84:7777/rpc"
-
-		privateKeyEd25519ForTransaction = "1228fcc08c02bfe100543a2581f60b0ad0e09f4c53e81641f09a70850880a256c1a19239600bf8293462d99f2ec19d5d1b443c760f9bdb4d720554585b26139a"
-		pathToPublicKeyFile             = "./public_key.in"
-
-		//bridgeContractPackageHash = "9299f58df67c2eff01e97f362996d35ab5393167e58c58360b1721cce95a7bbc"
-
-		// DragonNFT_contract_hash.
-		//bridgeContractPackageHash = "805b58313894dbef4ab184267484580fae7758d9591d6bc8da9b283cb4083cb8"
-		// market_contract_hash.
-		bridgeContractPackageHash = "277df7a58cfdc33a55176576ae9e5ab6c26b3b959aa9b18d5700cabea0c50598"
-	)
-
-	ctx := context.Background()
-	casperClient := contract.New(casperNodeAddress)
-
-	privateKeyForTransferSigningBytes, err := hex.DecodeString(privateKeyEd25519ForTransaction)
-	require.NoError(t, err)
-
-	// -----------
-	publicKey := make([]byte, 32)
-	copy(publicKey, privateKeyForTransferSigningBytes[32:])
-	pair := casper_ed25519.ParseKeyPair(publicKey, privateKeyForTransferSigningBytes[:32])
-
-	transfer := casper.NewTransfer(casperClient, func(b []byte) ([]byte, error) {
-		casperSignature := pair.Sign(b)
-		return casperSignature.SignatureData, nil
-	})
-
-	publicKeyFile, err := os.ReadFile(pathToPublicKeyFile)
-	require.NoError(t, err)
-	// -----------
-
-	txHash, err := transfer.SetAcceptOffer(ctx, casper.SetSignerRequest{
-		PublicKey:                   pair.PublicKey(),
-		ChainName:                   "casper-test",
-		StandardPaymentForBridgeOut: 2500000000, // 2.5 CSPR.
-		BridgeContractPackageHash:   bridgeContractPackageHash,
-		Value:                       string(publicKeyFile),
-	})
-	require.NoError(t, err)
-	require.NotEmpty(t, txHash)
-}
-
 func TestReadPrivateKeyFromFile(t *testing.T) {
 	t.Skip("for manual testing")
-	pr, err := casper_ed25519.ParsePrivateKeyFile("./new.pem")
+
+	pr, err := casper_ed25519.ParsePrivateKeyFile("./owner.pem")
 	require.NoError(t, err)
 	fmt.Println(hex.EncodeToString(pr))
 	t.Error()
 }
-
-func TestCasper_SetFinalListing(t *testing.T) {
-	var (
-		casperNodeAddress = "http://136.243.187.84:7777/rpc"
-
-		//privateKeyEd25519ForTransaction = "1228fcc08c02bfe100543a2581f60b0ad0e09f4c53e81641f09a70850880a256c1a19239600bf8293462d99f2ec19d5d1b443c760f9bdb4d720554585b26139a"
-		privateAccountKey = "861bf3e31b748978c0f86cf19eb33816d5dff7f10482f60bae0148ae52ec6826"
-		publicAccountKey  = "ad794c8f3da55845a5422506b4bd01ed1ae4e57378a82216d25d7351854b563d"
-		//publicAccountKey = "01ad794c8f3da55845a5422506b4bd01ed1ae4e57378a82216d25d7351854b563d"
-
-		//privateKeyEd25519ForTransaction = "861bf3e31b748978c0f86cf19eb33816d5dff7f10482f60bae0148ae52ec6826ad794c8f3da55845a5422506b4bd01ed1ae4e57378a82216d25d7351854b563d"
-
-		//pathToPrivat                    = "./UltimateMint4_secret_key.pem"
-		privateKeyEd25519ForTransaction = publicAccountKey + privateAccountKey
-
-		bridgeContractPackageHash = "277df7a58cfdc33a55176576ae9e5ab6c26b3b959aa9b18d5700cabea0c50598"
-		nftContractHash           = "contract-805b58313894dbef4ab184267484580fae7758d9591d6bc8da9b283cb4083cb8"
-		tokenID                   = "7a78a717-bf19-4e7b-902f-7d4f334cb4c6"
-		//tokenID = "1"
-	)
-
-	//fmt.Println("deploy-->", deploy)
-
-	ctx := context.Background()
-	casperClient := contract.New(casperNodeAddress)
-
-	// -----------
-	privateKeyForTransferSigningBytes, err := hex.DecodeString(privateKeyEd25519ForTransaction)
-	require.NoError(t, err)
-
-	privateAccountKeyBytes, err := hex.DecodeString(privateAccountKey)
-	require.NoError(t, err)
-	publicAccountKeyBytes, err := hex.DecodeString(publicAccountKey)
-	require.NoError(t, err)
-
-	fmt.Println("privateKeyForTransferSigningBytes-----_!!!!!!", privateKeyForTransferSigningBytes[32:])
-
-	pair := casper_ed25519.ParseKeyPair(publicAccountKeyBytes, privateAccountKeyBytes)
-
-	transfer := casper.NewTransfer(casperClient, func(b []byte) ([]byte, error) {
-		casperSignature := pair.Sign(b)
-		return casperSignature.SignatureData, nil
-	})
-	// -----------
-	txHash, err := transfer.SetFinalListing(ctx, casper.SetFinalListingRequest{
-		NftContractHash:             nftContractHash,
-		PublicKey:                   pair.PublicKey(),
-		ChainName:                   "casper-test",
-		StandardPaymentForBridgeOut: 2500000000, // 2.5 CSPR.
-		BridgeContractPackageHash:   bridgeContractPackageHash,
-		TokenID:                     tokenID,
-	})
-	require.NoError(t, err)
-	require.NotEmpty(t, txHash)
-}
-
-func TestCasper_SetFinalListing2(t *testing.T) {
-	var (
-		casperNodeAddress = "http://136.243.187.84:7777/rpc"
-
-		privateAccountKey = "861bf3e31b748978c0f86cf19eb33816d5dff7f10482f60bae0148ae52ec6826"
-		publicAccountKey  = "bbf5e5d84de2a446b11880bb5533e7ba85b3830171f3d457527fe60c49333f32"
-
-		bridgeContractPackageHash = "277df7a58cfdc33a55176576ae9e5ab6c26b3b959aa9b18d5700cabea0c50598"
-		nftContractHash           = "contract-805b58313894dbef4ab184267484580fae7758d9591d6bc8da9b283cb4083cb8"
-		tokenID                   = "7a78a717-bf19-4e7b-902f-7d4f334cb4c6"
-		//tokenID = "1"
-	)
-
-	ctx := context.Background()
-	casperClient := contract.New(casperNodeAddress)
-
-	// -----------
-	privateAccountKeyBytes, err := hex.DecodeString(privateAccountKey)
-	require.NoError(t, err)
-	publicAccountKeyBytes, err := hex.DecodeString(publicAccountKey)
-	require.NoError(t, err)
-
-	pair := casper_ed25519.ParseKeyPair(publicAccountKeyBytes, privateAccountKeyBytes)
-	//pair := casper_ed25519.ParseKeyPair(privateAccountKeyBytes, publicAccountKeyBytes)
-
-	transfer := casper.NewTransfer(casperClient, func(b []byte) ([]byte, error) {
-		casperSignature := pair.Sign(b)
-		return casperSignature.SignatureData, nil
-	})
-	// -----------
-	txHash, err := transfer.SetFinalListing(ctx, casper.SetFinalListingRequest{
-		NftContractHash:             nftContractHash,
-		PublicKey:                   pair.PublicKey(),
-		ChainName:                   "casper-test",
-		StandardPaymentForBridgeOut: 2500000000, // 2.5 CSPR.
-		BridgeContractPackageHash:   bridgeContractPackageHash,
-		TokenID:                     tokenID,
-	})
-	require.NoError(t, err)
-	require.NotEmpty(t, txHash)
-}
-
-////
-////
-////
-////
-////
-////
-////
-////
 
 func TestCasper_NftContract(t *testing.T) {
 	t.Skip("for manual testing")
@@ -242,6 +89,7 @@ func TestCasper_NftContract(t *testing.T) {
 
 func TestCasper_MarketContract(t *testing.T) {
 	t.Skip("for manual testing")
+
 	var (
 		casperNodeAddress = "http://116.202.169.210:7777/rpc"
 
@@ -346,6 +194,7 @@ func TestCasper_MarketContract(t *testing.T) {
 
 func TestCasper_TokensContract(t *testing.T) {
 	t.Skip("for manual testing")
+
 	var (
 		casperNodeAddress = "http://116.202.169.210:7777/rpc"
 
